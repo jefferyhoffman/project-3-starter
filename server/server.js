5311414d284f09aa5b60e5f8ca35865c20310cc3
William Harrison <alex@grahamsg.com>
2019-02-28 12:46:26 -0500

    stashing

--- server/server.js
+++ server/server.js
@@ -1,7 +1,3 @@
-/**
- * Project 3 Starter
- * UNC Charlotte Full-Stack Coding Bootcamp
- */
 
 //-- .env --------------------------------------------------------------------
 const path = require('path');
