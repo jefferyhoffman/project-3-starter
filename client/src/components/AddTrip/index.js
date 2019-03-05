0e6fbde6bd67a4409afc24116bfe6576233efd6e
J.McKinney <jsmckinney89@gmail.com>
2019-03-05 14:37:20 -0500

    condensed to single form and interacting with db

--- client/src/components/AddTrip/index.js
+++ client/src/components/AddTrip/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NewTripNavTabs from "./newTripNavTabs";
 import Form from "./Form";
-import ItinList from "./itinList";
-import Review from "./review"
+// import ItinList from "./itinList";
+// import Review from "./review"
 import "./style.css";
 
 
@@ -14,8 +14,8 @@ function AddNewTrip() {
         <h1 className="aann">Add a New Trip</h1> <hr/>
         <NewTripNavTabs />
         <Route exact path="/add" component={Form} />
-        <Route exact path="/add/additinerary" component={ItinList} />
-        <Route exact path="/add/review" component ={Review} />        
+        {/* <Route exact path="/add/additinerary" component={ItinList} />
+        <Route exact path="/add/review" component ={Review} />         */}
       </div>
     </Router>
   );
