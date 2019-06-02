955c181324fdc2e0fb34dcf0f9234077cda202fe
courtney manery <courtneymmanery@gmail.com>
2019-06-01 21:39:25 -0400

    added schema and seeds to db

---
+++ server/db/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE brand(
+    id INT NOT NULL AUTO_INCREMENT,
+    inventory_name VARCHAR(50) NOT NULL,
+    store BOOLEAN DEFAULT true,
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY (id)
+);
---
+++ server/db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO brand (inventory_name, store) VALUES ('id-01', true);
+INSERT INTO brand (inventory_name, store) VALUES ('id-02', true);
+INSERT INTO brand (inventory_name, store) VALUES ('id-03', true);
+INSERT INTO brand (inventory_name, store) VALUES ('id-04', true);
+INSERT INTO brand (inventory_name, store) VALUES ('id-05', true);
+INSERT INTO brand (inventory_name, store) VALUES ('id-06', true);
+INSERT INTO brand (inventory_name, store) VALUES ('id-07', true);
+INSERT INTO brand (inventory_name, store) VALUES ('id-08', true);
+INSERT INTO brand (inventory_name, store) VALUES ('id-09', true);
+INSERT INTO brand (inventory_name, store) VALUES ('id-10', true);
+INSERT INTO brand (inventory_name, store) VALUES ('id-11', true);
+INSERT INTO brand (inventory_name, store) VALUES ('id-12', true);
