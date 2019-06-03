08e0f6b184090344374ae5fc63ff901e13d926d9
Tyawo <48452575+Tyawo@users.noreply.github.com>
2019-06-03 16:17:57 -0400

    update apiroutes

--- server/controllers/api/itemsController.js
+++ server/controllers/api/itemsController.js
@@ -1,14 +1,26 @@
 const itemsController = require('express').Router();
 const db = require('../../models');
 
-itemsController.post('/api/items/:name', (req, res) => {
-    const { name, price, brand, quanitity, img } = req.body;
+// Create a new item
+itemsController.post('/:name', (req, res) => {
+    const { name, price, brand, quantity, img } = req.body;
   
-    db.watches.create({ name, price, brand, quanitity, img })
+    db.Items.create({ name, price, brand, quantity, img })
+      .then(item => res.json(item))
+      .catch(err => res.json(err));
+
+      db.Items.updateOne({ name, price, brand, quantity, img })
+      .then(item => res.json(item))
+      .catch(err => res.json(err));
+  });
+// Find all items
+  itemsController.get('/', (req, res) => {
+    const { name, price, brand, quantity, img } = req.body;
+  
+    db.Items.findAll({ name, price, brand, quantity, img })
       .then(item => res.json(item))
       .catch(err => res.json(err));
   });
 
-itemsController.get()
-  
+ 
   module.exports = itemsController;
---
+++ server/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO items ("name", "quantity", "price", "brand", "img")
+VALUES ("kk", 20, 200, "Sieko", "img")
