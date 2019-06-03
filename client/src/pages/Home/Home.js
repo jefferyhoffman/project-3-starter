16177d73b34739c95728d03dd11128fe39b0a1ee
courtney manery <courtneymmanery@gmail.com>
2019-06-03 10:12:11 -0400

    added images and seeds.sql

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -44,7 +44,7 @@ class HomePage extends Component {
                 <div className="card-body">
                   <h5 className="card-title">Card title</h5>
                   <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                  <a href="/" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
             </div>
