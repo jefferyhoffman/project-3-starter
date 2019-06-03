a8f5e7e638b567ae8cd67a8499ed7aeb9efec0a3
courtney manery <courtneymmanery@gmail.com>
2019-06-03 13:31:24 -0400

    pulling for updates

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 // import { Button } from 'reactstrap';
-
-import logo from './logo.svg';
-import id01 from './id-01.jpg';
 import "./style.css"
-
-
-
-
 
 class HomePage extends Component {
   constructor(props) {
@@ -16,82 +9,61 @@ class HomePage extends Component {
       counter: 0
     }
   }
-  handleclick = (event) => { 
+  handleclick = (event) => {
     console.log("click")
     let { counter } = this.state
     counter++
-    this.setState( { counter })
+    this.setState({ counter })
     console.log(this.state.counter)
   }
 
   render() {
     return (
-      <div className='Home'>
-        <div className='row'>
-          <div className='col'>
-            
-            {/* <img src={logo} className='App-logo' alt='logo' />
-            <p>
-              Edit <code>src/pages/Home.js</code> and save to reload.
-            </p> */}
-            <div class="titleSection"><h1>HELLO</h1></div>
-            <a
-              className='App-link'
-              href='https://reactjs.org'
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-              </a>
-            {/* <Button color="danger" onClick = {this.handleclick} > Danger!</Button> */}
-
-            <div class="card">
-              <img src="./client/src/Home/id-01.jpg" class="card-img-top" alt="product-1"/>
-                <div className="card-body">
-                  <h5 className="card-title">STORM Kong-X</h5>
-                  <img src={id01} className='App-product-1' alt='product-1' />
-                  <p className="card-text">The STORM Kong-X watch is a small thin strapped watch that is an effortlessly stylish addition to any wardrobe. 
-                  Made from stainless steel this watch combines modern design with the latest trends with its multi-strap of both bangle and mesh.
-                  Due to its small nature this is the perfect watch to stack with a statement bracelet. Water resistant to 50m.</p>
-                  <a href="/" class="btn btn-primary">Add to Shopping Cart</a>
-                </div>
       <div className="container-fluid">
         <div className="row" id="row1">
-          
+
         </div>
         <div className="row" id="row2">
-            <div className="col-6" id="row2sec1">
-        testing
+          <div className="col-6" id="row2sec1">
+            <div class="card">
+              <img src="./client/src/Home/id-01.jpg" class="card-img-top" alt="product-1" />
+              <div className="card-body">
+                <h5 className="card-title">STORM Kong-X</h5>
+                <img src="./id-01.jpg" alt="product-1"/>
+                <p className="card-text">The STORM Kong-X watch is a small thin strapped watch that is an effortlessly stylish addition to any wardrobe.
+                Made from stainless steel this watch combines modern design with the latest trends with its multi-strap of both bangle and mesh.
+                  Due to its small nature this is the perfect watch to stack with a statement bracelet. Water resistant to 50m.</p>
+                <a href="/" class="btn btn-primary">Add to Shopping Cart</a></div>
             </div>
             <div className="col-6" id="row2sec2">
-        testing
+              testing
             </div>
+          </div>
+          <div className="row" id="row3">
+            <div className="col-4" id="row3sec1">
+
+            </div>
+            <div className="col-4" id="row3sec2">
+
+            </div>
+            <div className="col-4" id="row3sec3">
+
+            </div>
+          </div>
+          <div className="row" id="row4">
+            <div className="col-6" id="row4sec1">
+
+            </div>
+            <div className="col-6" id="row4sec2">
+
+            </div>
+          </div>
+          </div>
         </div>
-        <div className="row" id="row3">
-          <div className="col-4" id="row3sec1">
-
-          </div>
-          <div className="col-4" id="row3sec2">
-
-          </div>
-          <div className="col-4" id="row3sec3">
-
-          </div>
-        </div>
-        <div className="row" id="row4">
-          <div className="col-6" id="row4sec1">
-
-          </div>
-          <div className="col-6" id="row4sec2">
-
-          </div>
-        </div>
-
-     </div>
 
 
-      );
-    }
-}
-    
+        );
+      }
+  }
+      
 export default HomePage;
