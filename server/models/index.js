a1284585774ed9608b12b8601153a3291f350ae8
Mitchel <mitchel.carson@gmail.com>
2019-06-02 20:06:57 -0400

    Working on User Auth

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 const User = require('./user');
-
+const Secret = require('./secret');
+const Service = require('./service');
 module.exports = {
   Users: require('./user'),
   Secrets: require('./secret')
