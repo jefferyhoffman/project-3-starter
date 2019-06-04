9b860465fd9f0e9f3122455a5c0bbc958f20574a
BlazePhoenix <donnel.ferguson@hotmail.com>
2019-06-03 20:54:13 -0400

    Completed API routes for GET/POST/PUT/DELETE

--- server/controllers/api/itemsController.js
+++ server/controllers/api/itemsController.js
@@ -1,26 +1,45 @@
 const itemsController = require('express').Router();
 const db = require('../../models');
 
-// Create a new item
-itemsController.post('/:name', (req, res) => {
-    const { name, price, brand, quantity, img } = req.body;
-  
-    db.Items.create({ name, price, brand, quantity, img })
-      .then(item => res.json(item))
-      .catch(err => res.json(err));
 
-      db.Items.updateOne({ name, price, brand, quantity, img })
-      .then(item => res.json(item))
-      .catch(err => res.json(err));
-  });
 // Find all items
-  itemsController.get('/', (req, res) => {
-    const { name, price, brand, quantity, img } = req.body;
-  
-    db.Items.findAll({ name, price, brand, quantity, img })
-      .then(item => res.json(item))
-      .catch(err => res.json(err));
-  });
+itemsController.get('/', (req, res) => {
+  const { name, price, brand, quantity, img } = req.body;
 
- 
-  module.exports = itemsController;
+  db.Item.findAll({ name, price, brand, quantity, img })
+    .then(item => res.json(item))
+    .catch(err => res.json(err));
+});
+
+// Create a new item
+itemsController.post('/create', (req, res) => {
+  const { name, price, brand, quantity, img } = req.body;
+
+  db.Item.create({ name, price, brand, quantity, img })
+    .then(item => res.json(item))
+    .catch(err => res.json(err));
+});
+
+// update existing item
+itemsController.put('/:name', (req, res) => {
+  const { name, price, brand, quantity, img } = req.body;
+
+  db.Item.update(
+    { name, price, brand, quantity, img },
+    { where: {name: req.params.name} })
+    .then(item => res.json(item))
+    .catch(err => res.json(err));
+});
+
+// delete existing item by ID
+itemsController.delete('/:id', (req, res) => {
+  const { name, price, brand, quantity, img } = req.body;
+
+  db.Item.destroy(
+    { where: {id: req.params.id} })
+    .then(item => res.json(item))
+    .catch(err => res.json(err));
+});
+
+
+module.exports = itemsController;
