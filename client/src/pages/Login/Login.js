1bd71ab69bd42a8c6fc29d023ba2aa79345abdb8
Mitchel <mitchel.carson@gmail.com>
2019-06-05 11:11:58 -0400

    Fixed merge request.

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -39,7 +39,7 @@ class Login extends Component {
   }
 
   render() {
-    const { from } = this.props.location.state || { from: { pathname: "/login" } };
+    const { from } = this.props.location.state || { from: { pathname: "/ClientHome" } };
     const { redirectToReferrer } = this.state;
 
     if (redirectToReferrer) {
