b731107b804d9b3120fa53da5177a429631b5496
Kong <kong_yang828@hotmail.com>
2019-06-06 23:07:03 -0400

    cart page edite

--- client/src/components/App/CartItem.js
+++ client/src/components/App/CartItem.js
@@ -10,12 +10,12 @@ export default class CartItem extends React.Component {
       <div className="cardProduct" style={{ marginBottom: "10px"}}>
         <div className="card-body">
           <h4 className="productName">{product.name}</h4>
-          <img src={product.img} alt="o" style={{width: '75px', height: '75px'}}/>
-          <h5 className="card-text"><small>price: </small>${product.price}</h5>
-          <span className="card-text text-success">
-              <small>Quantity: </small>{product.quantity}</span>
-          <button className="btn btn-sm btn-warning float-right" 
-              onClick={() => this.props.remove(product)}>Remove from cart</button>
+          <img id="cartItem" src={product.img} alt="o" style={{width: '100px', height: '100px'}}/>
+          <small className="priceTag">price: <span id="productPrice">${product.price}</span></small>
+          <small id="quantityTag" className="card-text">
+              Quantity: <span id="quantitySpan">{product.quantity}</span></small>
+          <button id="removeBtn" className="btn btn-sm btn-warning" 
+              onClick={() => this.props.remove(product)}>Remove this item</button><hr id="productHr" />
         </div>
       </div>
      )
