30c63cf301514a06b81c032a2e26d86ced472238
Jackie Zhou <Jacqueline.colleen88@gmail.com>
2019-06-06 20:45:39 -0400

    confirmation links working

--- client/src/pages/Checkout/CardSection.js
+++ client/src/pages/Checkout/CardSection.js
@@ -7,12 +7,12 @@ class CardSection extends React.Component {
   render() {
     return (
       <>
-        <label style={{ padding: '10px', float: 'right', width: '30%', backgroundColor: 'white', marginTop: '40px', marginRight: '20px', height: '200px', alignContent: 'center', textAlign: 'center', fontSize: '20px', borderRadius: '4px' }}>
+        <div className="card" style={{ padding: '10px', float: 'right', width: '30%', backgroundColor: 'white', marginTop: '40px', marginRight: '20px', height: '200px', alignContent: 'center', textAlign: 'center', fontSize: '20px', borderRadius: '4px' }}>
           Payment details
         <br />
           <CardElement style={{ base: { fontSize: '20px', margin: '30px', padding: '10px' } }} />
           <Link className="btn" style={{ margin: '40px', borderRadius: '4px' }} to='/Confirmation' onClick={this.toggleCollapse}> Confirm order</Link>
-        </label>
+        </div>
       </>
     );
   }
--- client/src/pages/Confirmation/Confirmation.js
+++ client/src/pages/Confirmation/Confirmation.js
@@ -1,31 +1,25 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
-import cardSection from "../../pages/Checkout/CardSection";
 
 
 class Confirmation extends Component {
 
     render() {
         return (
-            <div className="Container">
+            <div className="Container" style={{ padding: '10px', width: '40%', backgroundColor: 'white', marginLeft: '30%', merginRight: '30%', height: '400px', alignContent: 'center', textAlign: 'center', fontSize: '20px', borderRadius: '4px' }}>
                 <div className="card-body">
                     <div className="row">
-                        <div className="col-md-6">
+                        <div className="col">
                             <div className="card-header">
-                                <p className="heading lead">Order Placed!</p>
+                                <p className="heading lead" style={{fontSize: '30px'}}>Order Placed!</p>
                             </div>
                             <p className="text-center">
                                 <strong>Your order has been placed, a lawn care specialist will contact you 24hrs before your appointment.</strong></p>
-
-                            <div className="md-form">
-                                <textarea type="text" id="form79textarea" className="md-textarea form-control"
-                                    rows="3"></textarea>
+                                <Link className='btn btn-outline-warning waves-effect' to='/ClientHome'>Close</Link>
                             </div>
-                            <Link className='btn btn-outline-secondary waves-effect' to='/ClientHome'>Close</Link>
                         </div>
                     </div>
                 </div>
-            </div>
         )
     }
 }
