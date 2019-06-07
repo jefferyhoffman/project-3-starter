e336d9973777302e8d81ff4c73f9ebe3f481fb66
unknown <ruchita.gandhe@gmail.com>
2019-06-07 14:07:46 -0400

    adding search

--- client/src/pages/MyDrinks/MyDrinks.js
+++ client/src/pages/MyDrinks/MyDrinks.js
@@ -80,13 +80,22 @@ class MYDrinks extends Component {
         {drinks.map(drink=>{
           return (
             <>
-            <h3>{drink.name}</h3>
-            <p>{drink.instructions}</p>
-            <p>{drink.category}</p>
-            <p>{drink.kind}</p>
-            <p>{drink.ingredients_measurements}</p>
-            <p><img src={drink.image} alt={drink.name} /></p> 
-            <p>{drink.glass}</p>
+               <div className="card mb-3 container">
+              <div className="row no-gutters">
+                <div className="col-md-3">
+                  <img src={drink.image} alt={drink.name} className="card-img">
+                  </img>
+                </div>
+                <div className="col-md-7">
+                  <div className="card-body">
+                    <h5 className="card-title"><h3>{drink.name}</h3></h5>
+                    <p className="card-text"><h5>{drink.kind} - {drink.category}</h5> 
+                    <p className="card-text"> <p><strong>Ingredients:</strong> {drink.ingredients_measurements}</p><p><strong>Instructions:</strong> {drink.instructions}</p> <p><strong>Glass: </strong>{drink.glass}</p> </p></p> 
+                    <p className="card-text"><small class="text-muted"></small></p>
+                  </div>
+                </div>
+              </div>
+            </div>
             </>
           )
         })}
