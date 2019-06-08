bf52af262a98e2036b438b48f847f6e75d57f9a3
Dylan Wright <wright.dylan5@gmail.com>
2019-06-08 15:30:06 -0400

    user reviews

--- server/controllers/api/stripeController.js
+++ server/controllers/api/stripeController.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const routeHelpers = require('./stripeHelper');
 // const productQueries = require('../db/queries/products');
 
+
 // router.get('/', (req, res, next) => {
 //   return productQueries.getAllProducts()
 //   .then((products) => {
@@ -97,6 +98,6 @@ router.post('/:id/stripe', (req, res, next) => {
 module.exports = stripeController;
 
 
-stripe.createToken(card).then(function(result) {
-  // Handle result.error or result.token
-})
+// stripe.createToken(card).then(function(result) {
+//   // Handle result.error or result.token
+// })
