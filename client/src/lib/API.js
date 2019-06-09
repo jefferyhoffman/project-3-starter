56bdc7ab1be7ce87e276b3e844eb8ead1924a534
BlazePhoenix <donnel.ferguson@hotmail.com>
2019-06-09 14:16:40 -0400

    Edit API call

--- client/src/lib/API.js
+++ client/src/lib/API.js
@@ -14,8 +14,8 @@ export default {
       });
     },
 
-    register: function (email, password, name) {
-      return axios.post('/api/users/register', { email, password, name })
+    register: function (name, email, password) {
+      return axios.post('/api/users/register', { name, email, password })
     },
   },
 
@@ -23,15 +23,15 @@ export default {
     getItems: function () {
       return axios.get("/api/items");
     },
-    // Gets the item with the given id
+
     getItem: function (id) {
       return axios.get("/api/items/" + id);
     },
-    // Deletes the item with the given id, needs authentication
+
     deleteItem: function (id) {
       return axios.delete("/api/items/" + id);
     },
-    // Saves a item to the database, needs authentication
+
     saveItem: function (itemData) {
       return axios.post("/api/items", itemData);
     }
