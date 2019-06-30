0f3b0a6f121206952308dfe874a3773238db0e9f
unknown <ruchita.gandhe@gmail.com>
2019-06-30 15:24:08 -0400

    navbar

--- client/src/components/AuthDropdown/AuthDropdown.js
+++ client/src/components/AuthDropdown/AuthDropdown.js
@@ -3,6 +3,12 @@ import { Link } from 'react-router-dom';
 
 import AuthContext from '../../contexts/AuthContext';
 
+ const text = {
+ color: '#ff0000',
+//  color: 'black',
+}
+
+ 
 class AuthDropdown extends Component {
   static contextType = AuthContext;
 
@@ -30,8 +36,8 @@ class AuthDropdown extends Component {
 
     return (
       <li className="nav-item dropdown">
-      <button className="btn btn-link dropdown-toggle" onClick={this.toggleOpen} id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <div className="rounded-circle"  email={user.email} size={30} /> {user.email}
+      <button className="btn btn-link dropdown-toggle" style={text} onClick={this.toggleOpen} id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <div className="rounded-circle" email={user.email} size={30} /> {user.email}
       </button>
       <div className={dropdownMenuClass} aria-labelledby="navbarDropdown">
         <div className="dropdown-item" onClick={this.handleLogout}>Logout</div>
