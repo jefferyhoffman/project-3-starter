30d6de8cb328b7262be87cb1567a38a5a8afb568
David Martin <demartins37@yahoo.com>
2019-09-05 16:16:07 -0400

    storing

--- server/controllers/api/krystalControllers.js
+++ server/controllers/api/krystalControllers.js
@@ -1,6 +1,7 @@
 const db = require("../../models")
 const krystalControllers = require('express').Router();
-
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 krystalControllers.post("/", (req, res) => {
     customerInfo = req.body
@@ -12,7 +13,15 @@ krystalControllers.post("/", (req, res) => {
     db.Customers.create({customerInfo}).then(
         (err,response)=>{
             if(err) console.log(err)
-            console.log(response)
+            const msg = {
+                to: 'demartins37@yahoo.com',
+                from: 'demartins37@yahoo.com',
+                subject: 'Sending with Twilio SendGrid is Fun',
+                text: `${customerInfo.name, customerInfo.email} has contacted you with this message ${customerInfo.message}`,
+                html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+              };
+              sgMail.send(msg);
+
         }
     )
 })
