7f592cb04c900ca1ba7fd7751dd5c54545657fda
Brad Evans <kaloba4r@gmail.com>
2019-09-05 12:11:56 -0400

    minor changes to api

--- server/controllers/api/eventsORM.js
+++ server/controllers/api/eventsORM.js
@@ -6,7 +6,7 @@ module.exports = {
     console.log(req.query, 'req.query');
     // res.send('test response');
 
-    db.Event.find(req.query)
+    db.Events.find(req.query)
       .sort({ date: -1 })
       .then(dbModel => {
         console.log(dbModel, 'testing123');
@@ -15,22 +15,22 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Event.findById(req.params.id)
+    db.Events.findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Event.create(req.body)
+    db.Events.create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Event.findOneAndUpdate({ _id: req.params.id }, req.body)
+    db.Events.findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Event.findById({ _id: req.params.id })
+    db.Events.findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
