93e9facd2f0bdb5f30db31a0057c204ccd429f7c
Brad Evans <kaloba4r@gmail.com>
2019-09-05 10:18:06 -0400

    orm and api files added

--- server/controllers/api/eventsController.js
+++ server/controllers/api/eventsController.js
@@ -1,32 +1,21 @@
+const eventsController = require('express').Router();
+const eventsORM = require('./eventsORM');
+
 const db = require('../../models');
 
-// Defining methods for the EventsController
-module.exports = {
-  findAll: function(req, res) {
-    db.Event.find(req.query)
-      .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  findById: function(req, res) {
-    db.Event.findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  create: function(req, res) {
-    db.Event.create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  update: function(req, res) {
-    db.Event.findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  remove: function(req, res) {
-    db.Event.findById({ _id: req.params.id })
-      .then(dbModel => dbModel.remove())
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  }
-};
+eventsController.post('/', (req, res) => {
+  // const { email, password } = req.body;
+  // db.events.create({ email, password })
+  //   .then(user => res.json(user))
+  //   .catch(err => res.json(err));
+});
+
+eventsController.get('/', (req, res) => {
+  eventsORM.findAll(req, res);
+});
+
+eventsController.get('/:id', (req, res) => {
+  eventsORM.findById(req, res);
+});
+
+module.exports = eventsController;
---
+++ server/controllers/api/eventsORM.js
@@ -0,0 +1,37 @@
+const db = require('../../models');
+
+// Defining methods for the EventsController
+module.exports = {
+  findAll: function(req, res) {
+    console.log(req.query);
+    // res.send('test response');
+    db.Event.find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => {
+        console.log(dbModel);
+        res.json({});
+      })
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Event.findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Event.create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Event.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Event.findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
