3053d75260b6b534674bfe3b62263247aacb6bac
Brad Evans <kaloba4r@gmail.com>
2019-09-07 11:18:27 -0400

    input data types aligned to db model schema for post entries

--- server/models/event.js
+++ server/models/event.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
-  event_title: { type: String, required: true },
-  event_details: { type: String, required: true },
-  community: { type: String, required: true },
+  event_title: { type: String, required: false },
+  event_details: { type: String, required: false },
+  community: { type: String, required: false },
   category: { type: String, default: 'random' },
   event_place: { type: String },
   event_url: { type: String },
   html_url: { type: String },
   event_date: { type: Date, default: Date.now },
+  event_time: { type: String, default: Date.now },
   event_task: { type: String },
   isTaskCompleted: { type: Boolean, default: false }
 });
