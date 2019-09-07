e6cc3d4afeb87dad27c82bd83033d6b738363fa0
Brad Evans <kaloba4r@gmail.com>
2019-09-07 16:17:49 -0400

    updated post data in seedsDB

--- server/controllers/scripts/seedDB.js
+++ server/controllers/scripts/seedDB.js
@@ -516,6 +516,79 @@ const eventsData = [
     event_place: 'Midtown Park',
     community: 'Midtown',
     html_url: 'https://www.meetup.com/South-End-Social/'
+  },
+  {
+    _id: ObjectId('5d73c9d7ab2aa7245ceb4307'),
+    category: 'random',
+    isTaskCompleted: false,
+    event_title: 'South End Shuffle',
+    event_place: 'Wooden Robot Brewery',
+    event_date: ISODate('2019-09-05T00:00:00.000Z'),
+    html_url: 'https://runsignup.com/Race/NC/Charlotte/SouthEndShuffle5',
+    event_url:
+      'https://d368g9lw5ileu7.cloudfront.net/races/race76167-logo.bC1ztB.png',
+    event_details:
+      'There is something for everyone at the South End Shuffle 5K & Mile Run, the event that has become a South End neighborhood tradition! ',
+    event_time: '1567869399651',
+    __v: 0
+  }
+];
+
+const postsData = [
+  {
+    _id: ObjectId('5d72ac328bd033130a84c2a9'),
+    category: 'yard',
+    urgent: false,
+    help_needed: true,
+    offering: false,
+    cost_per_hour: 0,
+    cost_per_service: 15,
+    trade: false,
+    post_title: 'Looking for HS Student to help with Lawn',
+    post_details: 'Small Lawn on Tremont Lane in need of bi-weekly mowing',
+    __v: 0
+  },
+  {
+    _id: ObjectId('5d72adfa3c6a481394e50c7c'),
+    category: 'kids',
+    urgent: true,
+    help_needed: true,
+    offering: false,
+    cost_per_hour: 10,
+    cost_per_service: 0,
+    trade: false,
+    post_title: 'Baby Sitter needed for Tuesday Night!',
+    post_details:
+      'This is an urgent request for someone to help with babysitting 2 children (and one dog) for approx 3 hours on Tuesday night.  Appreciate the help if you can...!!!',
+    __v: 0
+  },
+  {
+    _id: ObjectId('5d72aef77f2f1d13d96e50d1'),
+    category: 'pets',
+    urgent: false,
+    help_needed: false,
+    offering: true,
+    cost_per_hour: 0,
+    cost_per_service: 18,
+    trade: false,
+    post_title: 'Dog Sitting',
+    post_details:
+      'I would like to help look after your pets while you are on summer vacation.  The rate of $18 per day includes walking them 3-4 times per day, and some chores around your house taking 30 minutes per day (e.g. bring in newspapers, feed the fish and cat etc. while I spend time with Fido.  My contact  info is Ricky at (704) 555-7654',
+    __v: 0
+  },
+  {
+    _id: ObjectId('5d72b0e37f2f1d13d96e50d2'),
+    category: 'car pooling',
+    urgent: false,
+    help_needed: false,
+    offering: true,
+    cost_per_hour: 0,
+    cost_per_service: 5,
+    trade: false,
+    post_title: 'Car Pooling Group',
+    post_details:
+      "Car Pooling Group from Matthews Reserve Apartments to Uptown.  We take turns daily.  Right now there are 5 of us but we'd like to get a small group of up to 10.  Everyone contributes $3 per ride toward collective gas costs or a social hour that we hold monthly.",
+    __v: 0
   }
 ];
 
