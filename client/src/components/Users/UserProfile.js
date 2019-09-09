d8a8d65b9a23ea9476a73b0401864e7e0f39246c
ZoeDj <studio3fun@gmail.com>
2019-09-09 16:07:40 -0400

    profil

--- client/src/components/Users/UserProfile.js
+++ client/src/components/Users/UserProfile.js
@@ -2,8 +2,21 @@ import React, { Component } from "react";
 import "./UserProfile.css";
 
 const UserProfile = props => {
+  const postImageArray = [
+    "https://www.stickpng.com/assets/images/58889250bc2fc2ef3a1860ab.png",
+    "https://www.stickpng.com/assets/images/588891f2bc2fc2ef3a1860a5.png"
+  ];
+  const postImage =
+    postImageArray[Math.floor(Math.random() * postImageArray.length)];
+
   return (
     <div className="UserProfile">
+      <img
+        src={postImage}
+        alt="url"
+        className="roundImg"
+        style={{ width: "60px " }}
+      />
       <h3 className="UserProfile_name">{props.user_name}</h3>
       <img className="UserProfile_img" src={props.user_url} alt="user-photo" />
     </div>
