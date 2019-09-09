92cc073b4e5e2425b8afcabb197e98b771b81f6b
ZoeDj <studio3fun@gmail.com>
2019-09-09 14:55:16 -0400

    profils page

--- client/src/components/Users/UserProfile.js
+++ client/src/components/Users/UserProfile.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import "./UserProfile.css";
 
 const UserProfile = props => {
   return (
-    <div>
-      <h5>{props.user_name}</h5>
-      <img src={props.user_url} alt="user-photo" />
+    <div className="UserProfile">
+      <h3 className="UserProfile_name">{props.user_name}</h3>
+      <img className="UserProfile_img" src={props.user_url} alt="user-photo" />
     </div>
   );
 };
