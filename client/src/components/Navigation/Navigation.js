db7ddc1f38fc0769bd25ad7fdc7c4fc8be79f12d
Tamara Golub <tgolub89@gmail.com>
2020-01-03 09:29:03 -0500

    made the background image on the main page

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import AuthContext from '../../contexts/AuthContext';
 import AuthDropdown from '../../components/AuthDropdown/AuthDropdown';
+import logo from "../../images/logo.svg"
 
 class Navigation extends Component {
   static contextType = AuthContext;
@@ -26,13 +27,16 @@ class Navigation extends Component {
     return (
       <div className='Navigation'>
         <nav className='navbar navbar-expand-lg navbar-light bg-light mb-3'>
-          <Link className='navbar-brand' to='#'>Project 3</Link>
+          <Link className='navbar-brand' to='#'>
+            <img className="logo" src={logo}/>
+            Landscaping
+            </Link>
           <button className={togglerClass} onClick={this.toggleCollapse} data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
             <span className='navbar-toggler-icon'></span>
           </button>
 
           <div className={targetClass} id='navbarSupportedContent'>
-            <ul className='navbar-nav mr-auto'>
+            {/* <ul className='navbar-nav mr-auto'>
               <li className='nav-item'>
                 <Link className='nav-link' to='/' onClick={this.toggleCollapse}>Home</Link>
               </li>
@@ -40,8 +44,15 @@ class Navigation extends Component {
                 <li className='nav-item'>
                   <Link className='nav-link' to='/secret' onClick={this.toggleCollapse}>Secret</Link>
                 </li>}
-            </ul>
-            <ul className='navbar-nav'>
+            </ul> */}
+            <ul className='navbar-nav ml-auto'>
+            <li className='nav-item'>
+                <Link className='nav-link' to='/' onClick={this.toggleCollapse}>Home</Link>
+              </li>
+              {user &&
+                <li className='nav-item'>
+                  <Link className='nav-link' to='/secret' onClick={this.toggleCollapse}>Secret</Link>
+                </li>}
               {user
                 ? <AuthDropdown onClick={this.toggleCollapse} />
                 : <>
