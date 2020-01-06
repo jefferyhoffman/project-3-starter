c4e58885966804b197c45fd5545ca7e4e279169a
John <phodgesfl@gmail.com>
2020-01-06 16:43:24 -0500

    added PlainJane card

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -57,7 +57,7 @@ class App extends Component {
           <div className='App'>
             <Switch>
             <Route exact path="/" component={Index} />
-            <PrivateRoute exact path="/UserDash" component={UserDash} />
+            <Route exact path="/UserDash" component={UserDash} />
             <Route exact path="/LoginPage" component={SignIn} />
             <Route exact path="/SignupPage" component={Signup}/>
             <Route exact path="/Studio" component={Studio}/>
---
+++ client/src/components/PlainJane/index.js
@@ -0,0 +1,2 @@
+import React from 'react';
+import { MDBNa, MDBNavbarBrand, MDBNavbarNav, MDBNavbarToggler, MDBCollapse, MDBNavItem, MDBNavLink, MDBContainer, MDBMask, MDBView, MDBRow, MDBCol } from 'mdbreact';
