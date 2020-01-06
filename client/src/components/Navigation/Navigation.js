d6ce9bb789d363c1cce59bc985b3baa17bca3f19
Josh Eskew <jeskew82@gmail.com>
2020-01-06 14:49:06 -0500

    made changes to css

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import './navigation.css';
 import AuthContext from '../../contexts/AuthContext';
 import AuthDropdown from '../../components/AuthDropdown/AuthDropdown';
-import logo from "../../images/transaparentlogo.png"
+import logo from "../../images/BGM-logo.jpg"
 
 class Navigation extends Component {
   static contextType = AuthContext;
@@ -27,11 +27,11 @@ class Navigation extends Component {
 
     return (
       <div className='Navigation'>
-        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+        <nav className='navbar navbar-expand-lg '>
           <Link className='navbar-brand' to='#'>
-            <img className="logo" src={logo}/>
-            
-            </Link>
+            <img className="logo" src={logo} />
+
+          </Link>
           <button className={togglerClass} onClick={this.toggleCollapse} data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
             <span className='navbar-toggler-icon'></span>
           </button>
@@ -47,8 +47,11 @@ class Navigation extends Component {
                 </li>}
             </ul> */}
             <ul className='navbar-nav ml-auto'>
-            <li className='nav-item'>
+              <li className='nav-item'>
                 <Link className='nav-link' to='/' onClick={this.toggleCollapse}>Home</Link>
+              </li>
+              <li className='nav-item'>
+                <Link className='nav-link' to='/' onClick={this.toggleCollapse}>Services</Link>
               </li>
               {user &&
                 <li className='nav-item'>
@@ -59,7 +62,7 @@ class Navigation extends Component {
                 : <>
                   <li className='nav-item'><Link className='nav-link' to='/login' onClick={this.toggleCollapse}>Login</Link></li>
                   <li className='nav-item'><Link className='nav-link' to='/register' onClick={this.toggleCollapse}>Register</Link></li>
-                  </>}
+                </>}
             </ul>
           </div>
         </nav>
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -13,37 +13,23 @@ class HomePage extends Component {
       <div className="Home">
         <div className="container">
           <div className="row">
-            {/* <div className="col">
-              <p>
-                Scroll Up and Down this page to see the parallax scrolling
-                effect.{" "}
-              </p>
-            </div> */}
+            
           </div>
         </div>
         <div className="parallax parallax-1">
-          <h1>This is the best feature ever!!!</h1>
+          
         </div>
-        <div
-          style={{
-            height: "400px",
-            backgroundColor: "#ffc41f",
-            fontSize: "36px",
-            zIndex: "1000"
-
-
-          }}
-        >
+        
           <div className="container">
             <div className="row">
               <div className="col">
-                <div>
+                {/* <div>
                   Our mission is to provide our clients with the highest level of quality services at fair and competitive prices.  We believe that by providing our clients with exceptional workmanship, extraordinary service and professional integrity, we’ll ensure the longevity of our company through repeat and referral business
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
-        </div>
+        
         <div className="parallax parallax-2"></div>
 
         <div className="container">
