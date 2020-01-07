6bea9620347551d03ecd29ad6f7c9f4e59ff0da1
Michael Trpak <mrtrpak3@gmail.com>
2020-01-07 12:30:38 -0500

    updated studio page to be class components and updated API to be able to save card data once table is created

--- client/src/components/NewCardForm/NewCardForm.js
+++ client/src/components/NewCardForm/NewCardForm.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBCard, MDBCardBody } from 'mdbreact';
 
 const createCard = e => {
     console.log("hi")
 }
 
-const NewCardForm = () => {
-    return (
-        <MDBContainer>
+class NewCardForm extends Component {
+    state = {
+        name: "",
+        company: "",
+        email: "",
+        website: "",
+        phoneNum: ""
+    }
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleSubmit = event => {
+        const { name, company, email, website, phoneNum } = this.state;
+
+        this.props.onSubmit(name, company, email, website, phoneNum);
+        console.log(name, company, email, website, phoneNum);
+        event.preventDefault();
+    }
+
+    render() {
+        const { name, company, email, website, phoneNum } = this.state;
+        return (
+            <MDBContainer>
             <MDBRow>
                 <MDBCol>
                     <MDBCard>
@@ -17,15 +43,21 @@ const NewCardForm = () => {
                                 <div className="grey-text text-left">
                                     <MDBInput
                                         label="Your Name"
+                                        name="name"
+                                        value={name}
+                                        onChange={this.handleInputChange}
                                         group
                                         icon="user"
                                         type="text"
                                         validate
                                         error="wrong"
                                         success="right"
-                                    />
+                                        />
                                     <MDBInput
                                         label="Your Company Name (optional)"
+                                        name="company"
+                                        value={company}
+                                        onChange={this.handleInputChange}
                                         group
                                         icon="building"
                                         type="text"
@@ -34,35 +66,44 @@ const NewCardForm = () => {
                                     />
 
                                     <MDBInput
-                                        label="Your Email"
+                                        label="Your Email (doesn't have to be sign in email)"
+                                        name="email"
+                                        value={email}
+                                        onChange={this.handleInputChange}
                                         icon="envelope"
                                         group
                                         type="email"
                                         validate
                                         error="wrong"
                                         success="right"
-                                    />
+                                        />
                                     <MDBInput
                                         label="Your Website"
+                                        name="website"
+                                        value={website}
+                                        onChange={this.handleInputChange}
                                         icon="atlas"
                                         group
                                         type="text"
                                         validate
                                         error="wrong"
                                         success="right"
-                                    />
+                                        />
                                     <MDBInput
                                         label="Your Phone"
+                                        name="phoneNum"
+                                        value={phoneNum}
+                                        onChange={this.handleInputChange}
                                         icon="phone"
                                         group
                                         type="text"
                                         validate
-                                    />
+                                        />
                                 </div>
                                 <div className="text-center py-4 mt-3">
-                                    <MDBBtn color="orange" type="submit" onSubmit={createCard()}>
+                                    <MDBBtn color="orange" rounded type="submit">
                                         Create Card
-                                </MDBBtn>
+                                    </MDBBtn>
                                 </div>
                             </form>
                         </MDBCardBody>
@@ -70,7 +111,8 @@ const NewCardForm = () => {
                 </MDBCol>
             </MDBRow>
         </MDBContainer>
-    );
+        );
+    }
 };
 
 export default NewCardForm;
--- client/src/pages/Studio/index.js
+++ client/src/pages/Studio/index.js
@@ -1,28 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {MDBRow, MDBCol, MDBContainer} from "mdbreact";
 import NewCardForm from "../../components/NewCardForm/NewCardForm.js";
 import IndexNav from "../../components/IndexNAV/index.js"
 import "./index.css"
+import API from '../../lib/API.js';
 
 
 
 
-function Studio(){
-    return (
+class Studio extends Component{
+    state = {
+        error: ""
+    }
+
+    handleSubmit = (name, company, email, website, phoneNum) => {
+        if (name === "" || email === "" || website === "" || phoneNum === "") {
+            return this.setState({ error: "Please enter your name, email, website and phone number" });
+        }
+
+        API.newCards.create(name, company, email, website, phoneNum)
+            .then(response => response.data)
+            .catch(err => this.setState({ error: err.message }));
+    }
+
+    render() {
+        return (
         <div className="studioBcg">
         <React.Fragment>
-        <IndexNav/>
+            <IndexNav/>
             <br/>
             <MDBRow>
                 <MDBCol size="2"></MDBCol>
                 <MDBCol size="8" >
-                    <NewCardForm />
+                    <NewCardForm onSubmit={this.handleSubmit}/>
                 </MDBCol>
                 <MDBCol size="2" id="rightColumn"></MDBCol>
-                </MDBRow> 
+            </MDBRow> 
         </React.Fragment>
         </div>
-    )
+        )
+    }
 }
 
 export default Studio;
