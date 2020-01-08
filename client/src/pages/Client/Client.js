520e172e54c00ff542529e87ae8322e8d2a90b0e
Josh Eskew <jeskew82@gmail.com>
2020-01-08 14:16:14 -0500

    made progress with client.js

--- client/src/pages/Client/Client.js
+++ client/src/pages/Client/Client.js
@@ -1,53 +1,84 @@
-import React, { Component } from 'react';
 
-class Form extends Component {
-  state = {
-    property: ["2020 Main St", "100 Washington Ave", "450 West Blvd", "1330 Green St", "78 Bigsby Rd"],
-    date: "",
-    time: "",
-    task: "",
-    comments: ""
-  };
-
-  render() {
-    return (
-      <div>
-        
-        <div class="dropdown">
-          <button class="dropbtn">Dropdown
-          <i class="fa fa-caret-down"></i>
-          </button>
-          <div class="dropdown-content">
-            {this.state.property.map(property => (
-              <p>{property}</p>
-            ))}
-          </div>
-        </div>
-
-        <input
-          value={this.state.comments}
-          name="comments"
-          onChange={this.handleInputChange}
-          type="text"
-          placeholder="Comments"
-        />
-        <button onClick={this.handleFormSubmit}>Submit</button>
-      </div>
-    );
-  }
-}
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import AuthContext from "../../contexts/AuthContext";
 
 const Client = () => {
-    const { user }  = useContext(AuthContext);
-    
-    if (!user || user.role !== "Client") {
-        return <Redirect to = "/" />
-    }
+  const { user } = useContext(AuthContext);
 
-    return <h1>Welcome Back</h1>;
+  // ["2020 Main St", "100 Washington Ave", "450 West Blvd", "1330 Green St", "78 Bigsby Rd"]
+
+  const [properties, setProperties] = useState([
+    //get rid of dummy date after db is populated
+    {
+      propertyId: 1,
+      address: "2020 Main St",
+      date: "date here",
+      task: "task sample",
+      employeeComments:["emp comment 1", "emp comment 2"],
+      clientComments:["client comment 1", "client comment 2"],
+      photo: "www.google.com"
+    },
+    {
+      propertyId: 2,
+      address: "450 West Blvd",
+      date: "date here",
+      task: "task sample",
+      employeeComments:["emp comment 1", "emp comment 2"],
+      clientComments:["client comment 1", "client comment 2"],
+      photo: "www.google.com"
+    }
+]);
+
+  const [selectedPropertyIndex, setSelectedPropertyIndex] = useState( properties[0].propertyId -1 );
+
+  // useEffect(()=>{
+  //  api call to get properties here
+  //  setProperties(apiDataHere)
+  // },[])
+
+  const handleChange = event => {
+    setSelectedPropertyIndex(parseInt(event.target.value));
+  }
+
+  const handleSubmit = event => {
+    alert(`
+    Selected Property ID: ${selectedPropertyIndex + 1}
+    Selected Property Address: ${properties[selectedPropertyIndex].address}
+    Selected Property Date: ${properties[selectedPropertyIndex].date}
+    Selected Property Task: ${properties[selectedPropertyIndex].task}
+    Selected Property Employee Comments: ${properties[selectedPropertyIndex].employeeComments}
+    Selected Property Client Comments: ${properties[selectedPropertyIndex].clientComments}
+    Selected Property photo url: ${properties[selectedPropertyIndex].photo}
+    `);
+    event.preventDefault();
+  }
+
+
+  // if (!user || user.role !== "Client") {
+  //     return <Redirect to = "/" />
+  // }
+  //   return ( EVERYTHING IN THE RETURN BELOW )
+
+  return (
+    <div style={{marginTop:"20vh"}}>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Select your property
+          <select value={selectedPropertyIndex} onChange={handleChange}>
+            {properties.length && properties.map(property =>  <option value={property.propertyId - 1}>{property.address}</option>)}
+          </select>
+        </label>
+        <input type="submit" value="Select Property" />
+      </form>
+
+
+      {/* 
+        build a form with inputs to modify each property detail
+        
+      */}
+    </div>
+  )
 };
 
 export default Client
