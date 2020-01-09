79b0459b42a8abb6b3783ab51c15661f11cc0f47
darceym <darcey.meus@gmail.com>
2020-01-09 13:05:02 -0500

    styling services page

--- client/src/pages/Services/Services.js
+++ client/src/pages/Services/Services.js
@@ -1,2 +1,19 @@
 import React, { Component } from 'react';
 
+class ServicesPage extends Component {
+render() {
+    return (
+      <div className='Services'>
+        <div className='row'>
+          <div className='col'>
+                  <p>Shh, the secret is...</p>
+                  <p>hello</p>
+                </div>
+          </div>
+        </div>
+      
+    );
+  }
+}
+
+export default Services;
