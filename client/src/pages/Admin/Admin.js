0432eb07bd3a12092fdf722cd2b59a23293c8f7e
Dan Shelly <djshelly24@gmail.com>
2020-01-14 14:20:40 -0500

    Moved message for admin submissions

--- client/src/pages/Admin/Admin.js
+++ client/src/pages/Admin/Admin.js
@@ -5,7 +5,7 @@ import API from "../../lib/API";
 import FlashMessage from "react-flash-message";
 
 const Admin = () => {
-    const { user,authToken } = useContext(AuthContext);
+    const { user, authToken } = useContext(AuthContext);
     const [users, setUsers] = useState([]);
     const [selectedUser, setSelectedUser] = useState("select user")
     const [role, setRole] = useState("set role")
@@ -24,17 +24,18 @@ const Admin = () => {
                 setUsers(res.data.users)
             }
             )
-            .catch(err => console.log(err));
-            API.Properties.create().then(response => {
 
-                setMessage("Successfully registered user.");
-                setSubmitted(true);
-            })
     }
 
     const handleSub = (event) => {
         event.preventDefault()
-        API.Users.updateRole(authToken, selectedUser, role).then(()=>console.log("success"))
+        API.Users.updateRole(authToken, selectedUser, role).then(() => console.log("success"))
+            .catch(err => console.log(err));
+        API.Properties.create().then(response => {
+
+            setMessage("Successfully registered user.");
+            setSubmitted(true);
+        })
     }
     if (!user || user.role !== "Admin") {
         return <Redirect to="/" />
@@ -43,48 +44,49 @@ const Admin = () => {
     return (
 
         <div className="Login">
-            {submitted && (
-                  <div className="row" style={{ zIndex: 10 }}>
-                    <div
-                      className="col centeredContent"
-                      style={{ marginTop: "-100px" }}
-                    >
-                      <FlashMessage duration={5000}>
-                        <div className="alert alert-success mb-3 flash" role="alert">
-                          {message}
-                        </div>
-                      </FlashMessage>
-                    </div>
-                  </div>
-                )}
-            
-
-                <h1 className="admin">Hello Admin</h1>
-                <form onSubmit={handleSub}>
 
 
-                    <select id="use" onChange={event=> setSelectedUser(event.target.value)} value={selectedUser} name="setSelectedUser">
+
+            <h1 className="admin">Hello Admin</h1>
+            <form onSubmit={handleSub}>
+
+
+                <select id="use" onChange={event => setSelectedUser(event.target.value)} value={selectedUser} name="setSelectedUser">
                     <option className="user" key={123452435} value="select user">Select User</option>
-                        {users.map(currentUser => {
-                            return (<option
-                                key={currentUser._id}
-                                value={currentUser._id}>
-                                {currentUser.lastName}, {currentUser.firstName}
-                            </option>)
-                        })}
+                    {users.map(currentUser => {
+                        return (<option
+                            key={currentUser._id}
+                            value={currentUser._id}>
+                            {currentUser.lastName}, {currentUser.firstName}
+                        </option>)
+                    })}
 
-                    </select>
-                    <select id="role" onChange={event=>setRole(event.target.value)}>
-                        <option key={1432345} value="set role">Set Role</option>
-                        <option key={1657457} value="Client">Client</option>
-                        <option key={287989} value="Employee">Employee</option>
-                        <option key={36457} value="Admin">Admin</option>
-                    </select>
-                    <br />
-                    <input type="submit" className="submitBtn"/>
-                </form>
+                </select>
+                <select id="role" onChange={event => setRole(event.target.value)}>
+                    <option key={1432345} value="set role">Set Role</option>
+                    <option key={1657457} value="Client">Client</option>
+                    <option key={287989} value="Employee">Employee</option>
+                    <option key={36457} value="Admin">Admin</option>
+                </select>
+                <br />
+                <input type="submit" className="submitBtn" />
+                {submitted && (
+                    <div className="row" style={{ zIndex: 10 }}>
+                        <div
+                            className="col centeredContent"
+                            style={{ marginTop: "-90px" }}
+                        >
+                            <FlashMessage duration={5000}>
+                                <div className="alert alert-success mb-3 flash" role="alert">
+                                    {message}
+                                </div>
+                            </FlashMessage>
+                        </div>
+                    </div>
+                )}
+            </form>
 
-            
+
 
         </div>
     );
