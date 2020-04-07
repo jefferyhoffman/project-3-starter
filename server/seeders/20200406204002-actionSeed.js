c376b2d438b8e017df985691389bff42b7f6aede
ccrum292 <ccrum292@gmail.com>
2020-04-06 21:10:21 -0400

    additional seeding data was added

--- server/seeders/20200406204002-actionSeed.js
+++ server/seeders/20200406204002-actionSeed.js
@@ -18,6 +18,33 @@ module.exports = {
           createdAt: new Date,
           updatedAt: new Date
         },
+        {
+          category: "Travel",
+          name: "Regularly Biking to Work/School",
+          description:
+            "More than 100 gallons of gasoline can be saved every year by the average commuter choosing to ride their bike to work.",
+          points: 60,
+          createdAt: new Date,
+          updatedAt: new Date
+        },
+        {
+          category: "Home",
+          name: "Use of LED bulbs",
+          description:
+            "Replacing one incandescent bulb with an LED bulb can reduce a home's greenhouse gas emissions by half a ton.",
+          points: 20,
+          createdAt: new Date,
+          updatedAt: new Date
+        },
+        {
+          category: "Consumable Items",
+          name: "Buying Clothing from Thrift Stores",
+          description:
+            "The average pair of jeans takes 1,800 gallons of water to produce. By wearing gently used clothing, the intesive process of producing textiles is, on the whole, reduced.",
+          points: 20,
+          createdAt: new Date,
+          updatedAt: new Date
+        },
       ],
       {}
     );
