05a2c0d1424efd4677151c6dd1eb9ab30106c612
Kevin Washburn <kevincwashburn@gmail.com>
2020-04-07 11:24:51 -0400

    added board, column schemas. revised card schema

---
+++ server/models/column.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const columnSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  boardId: {
+      type: Schema.Types.ObjectId,
+      ref: "Board",
+      required: true
+  }
+});
+
+const Column = mongoose.model("Column", columnSchema);
+
+module.exports = Column;
