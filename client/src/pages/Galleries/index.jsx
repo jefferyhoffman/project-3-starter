cb0a77f29aec1a368741db6e9f7927c008fa3ca7
Kevin Parmar <parmarkevinb@gmail.com>
2020-04-09 11:14:57 -0400

    Added upload form and key to galleries page

--- client/src/pages/Galleries/index.jsx
+++ client/src/pages/Galleries/index.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import Gallery from '../../components/Gallery';
+import UploadForm from '../../components/UploadForm'
 import './style.css'
 
 function Galleries(props) {
+     
     return (
         <div>
-            <div className='button'>
-                <input type='file' id='upload'/>
-            </div>
+            <UploadForm />
             <div className='galleries'>
-                {props.galleries.map(gallery => 
-                    <Gallery id={gallery.id} images={gallery.images} name={gallery.name}/>
+                {props.galleries.map((gallery, index) => 
+                    <Gallery 
+                        id={gallery.id}
+                        key={index} 
+                        images={gallery.images} 
+                        name={gallery.name}/>
                 )}
             </div>
         </div>
