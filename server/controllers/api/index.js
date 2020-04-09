dee455776c26f907ec830ef2589b8d77735f0305
Zach M <zatchmdws@gmail.com>
2020-04-09 14:54:10 -0400

    Got routes working for both replies and threads, could not renname due to issue

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 const apiControllers = require('express').Router();
 const threadRoutes = require("./thread");
-const postRoutes = require("./post");
+const replyRoutes = require("./post");
 
 //Thread routes
 router.use("/thread", threadRoutes);
 
 //Post routes
-router.use("/reply", postRoutes);
+router.use("/reply", replyRoutes);
 
 
 apiControllers.use('/users', require('./usersController'));
--- server/controllers/api/postController.js
+++ server/controllers/api/postController.js
@@ -1,4 +1,4 @@
-const db = require("../../models/");
+const db = require("../../models");
 
 module.exports = { 
     findAll: function(req, res) { 
