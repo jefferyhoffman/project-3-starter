6464513b06333d33f64c5c4bc593d10ffa7197d2
ccrum292 <ccrum292@gmail.com>
2020-04-08 20:58:32 -0400

    initial sendgrid setup

---
+++ server/lib/emailSendgrid.js
@@ -0,0 +1,13 @@
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+require("dotenv").config;
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'ccrum292@gmail.com',
+  from: 'ccrum292@gmail.com',
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
