bd9fdb6e71cbf1bddddc23033e9ddd4d5bb5a82f
Julie Berryhill <juliesellsnc@gmail.com>
2020-04-08 22:18:39 -0400

    fixed database

--- client/src/pages/User/index.js
+++ client/src/pages/User/index.js
@@ -3,13 +3,19 @@ import { Accordion, Card, Button } from "react-bootstrap";
 import AuthContext from "../../contexts/AuthContext";
 import Selected from "../../components/SelectedChallenges";
 import API from "../../lib/API";
+
 const User = (props) => {
   const userInfo = useContext(AuthContext);
   const [theSelected, setTheSelected] = useState([]);
+  const [allActions, setAllActions] = useState();
+
   useEffect(() => {
+    const allTheActions = API.Actions.getAll();
+    setAllActions(allTheActions);
     const selected = API.Actions.getSelected();
     setTheSelected(selected);
   }, []);
+  console.log(allActions);
   console.log(userInfo);
   return (
     <>
@@ -19,7 +25,7 @@ const User = (props) => {
         <Card>
           <Card.Header>
             <Accordion.Toggle as={Button} variant="link" eventKey="0">
-              Click me!
+              Food Actions Category
             </Accordion.Toggle>
           </Card.Header>
           <Accordion.Collapse eventKey="0">
@@ -30,38 +36,46 @@ const User = (props) => {
               >
                 +
               </span>{" "}
-              Hello! I'm the body
+              Action Item 1
             </Card.Body>
           </Accordion.Collapse>
         </Card>
         <Card>
           <Card.Header>
             <Accordion.Toggle as={Button} variant="link" eventKey="1">
-              Click me!
+              Travel Actions Category
             </Accordion.Toggle>
           </Card.Header>
           <Accordion.Collapse eventKey="1">
-            <Card.Body>Hello! I'm another body</Card.Body>
+            <Card.Body>
+              <span
+                style={{ cursor: "pointer" }}
+                onClick={() => alert("clicked")}
+              >
+                +
+              </span>{" "}
+              Action Item 1
+            </Card.Body>
           </Accordion.Collapse>
         </Card>
         <Card>
           <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="1">
-              Click me!
+            <Accordion.Toggle as={Button} variant="link" eventKey="2">
+              Home Actions Category
             </Accordion.Toggle>
           </Card.Header>
-          <Accordion.Collapse eventKey="1">
-            <Card.Body>Hello! I'm another body</Card.Body>
+          <Accordion.Collapse eventKey="2">
+            <Card.Body>Action Item 1</Card.Body>
           </Accordion.Collapse>
         </Card>
         <Card>
           <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="1">
-              Click me!
+            <Accordion.Toggle as={Button} variant="link" eventKey="3">
+              Consumable Actions Category
             </Accordion.Toggle>
           </Card.Header>
-          <Accordion.Collapse eventKey="1">
-            <Card.Body>Hello! I'm another body</Card.Body>
+          <Accordion.Collapse eventKey="3">
+            <Card.Body>Action Item 1</Card.Body>
           </Accordion.Collapse>
         </Card>
       </Accordion>
