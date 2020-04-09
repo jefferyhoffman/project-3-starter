f7dadde7435091cee7d54b5734f135d9dbe4a88e
Alex <alexstiles126@gmail.com>
2020-04-09 10:18:57 -0400

    WIP, added profile page components on client-side

--- client/src/pages/UserProfile/profile.js
+++ client/src/pages/UserProfile/profile.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 
     class Profile extends Component {
-    static contextType = AuthContext;
-  
-    state = {
-      collapsed: true
-    }
   
     // toggleCollapse = () => {
     //   this.setState({
