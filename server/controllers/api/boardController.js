1c9ba4f813280ca471a10c3965c5109b366bdbd3
Warren Shehan <wwshehan@gmail.com>
2020-04-10 00:51:07 -0400

    fixing merge issues

--- server/controllers/api/boardController.js
+++ server/controllers/api/boardController.js
@@ -48,24 +48,6 @@ boardController.get("/userBoard", JWTVerifier, (req, res) => {
     .catch((err) => res.json(err));
 });
 
-<<<<<<< HEAD
-boardController.post("/:id/columns/:index/cards", JWTVerifier, (req, res) => {
-  db.Board.findById(req.params.id)
-    .then((board) => {
-      if (!board) {
-        throw new Error("Invalid board ID");
-      }
-      
-      const index = parseInt(req.params.index);
-
-      if (!board.columns[index]) {
-        throw new Error("Invalid column index");
-      }
-
-      board.columns[index].cards.push(req.body);
-      return board.save();
-    })
-=======
 // UPDATE BOARD
 boardController.put("/:id", ({ params, body }, res) => {
   db.Board.findByIdAndUpdate(
@@ -78,13 +60,10 @@ boardController.put("/:id", ({ params, body }, res) => {
       },
     }
   )
->>>>>>> 114889959e72e70210db7d076d99dbc6621bfe1e
     .then((updatedBoard) => res.json(updatedBoard))
     .catch((err) => res.json(err));
 });
 
-<<<<<<< HEAD
-=======
 // -------------------------------------------------------------------------
 
 // CREATE COLUMN
@@ -120,17 +99,23 @@ boardController.delete("/:id/columns/:column", ({ params }, res) => {
 });
 
 // -------------------------------------------------------------------------
-
 // CREATE CARD
-boardController.post("/:id/columns/:column/cards", (req, res) => {
-  db.Card.create({
-    title: req.body.title,
-    body: req.body.body,
-    // priority: req.body.priority,
-  })
-    .then((dbCard) => res.json(dbCard))
-    .catch((err) => res.json(err));
-});
+boardController.post("/:id/columns/:index/cards", JWTVerifier, (req, res) => {
+  db.Board.findById(req.params.id)
+    .then((board) => {
+      if (!board) {
+        throw new Error("Invalid board ID");
+      }
+      
+      const index = parseInt(req.params.index);
+
+      if (!board.columns[index]) {
+        throw new Error("Invalid column index");
+      }
+
+      board.columns[index].cards.push(req.body);
+      return board.save();
+    })
 
 // UPDATE CARD
 boardController.put("/:id/columns/:column/cards/:card", ({ params, body }, res) => {
@@ -159,6 +144,4 @@ boardController.delete("/:id/columns/:column/cards/:card", ({ params }, res) => 
     .catch((err) => res.json(err));
 });
 
->>>>>>> 114889959e72e70210db7d076d99dbc6621bfe1e
 module.exports = boardController;
-
