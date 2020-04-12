aa61e0478b00a97d34e09f485fc080c067aca333
Kevin Parmar <parmarkevinb@gmail.com>
2020-04-11 23:20:39 -0400

    Added fade-in class to nav links

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import AuthContext from '../../contexts/AuthContext';
 import AuthDropdown from '../../components/AuthDropdown/AuthDropdown';
 
+import './style.css'
+
 class Navigation extends Component {
   static contextType = AuthContext;
 
@@ -25,8 +27,8 @@ class Navigation extends Component {
 
     return (
       <div className='Navigation'>
-        <nav className='navbar navbar-expand-lg navbar-light bg-light mb-3'>
-          <Link className='navbar-brand' to='#'>Baton Pass</Link>
+        <nav className='navbar navbar-expand-lg navbar-light mb-3'>
+          <Link className='navbar-brand fade-in' to='#'>Baton Pass</Link>
           <button className={togglerClass} onClick={this.toggleCollapse} data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
             <span className='navbar-toggler-icon'></span>
           </button>
@@ -34,18 +36,18 @@ class Navigation extends Component {
           <div className={targetClass} id='navbarSupportedContent'>
             <ul className='navbar-nav mr-auto'>
               <li className='nav-item'>
-                <Link className='nav-link' to='/' onClick={this.toggleCollapse}>Home</Link>
+                <Link className='nav-link fade-in' to='/' onClick={this.toggleCollapse}>Home</Link>
               </li>
               <li className='nav-item'>
-                <Link className='nav-link' to='/galleries' onClick={this.toggleCollapse}>Galleries</Link>
+                <Link className='nav-link fade-in' to='/galleries' onClick={this.toggleCollapse}>Galleries</Link>
               </li>
             </ul>
             <ul className='navbar-nav'>
               {user
                 ? <AuthDropdown onClick={this.toggleCollapse} />
                 : <>
-                  <li className='nav-item'><Link className='nav-link' to='/login' onClick={this.toggleCollapse}>Login</Link></li>
-                  <li className='nav-item'><Link className='nav-link' to='/register' onClick={this.toggleCollapse}>Register</Link></li>
+                  <li className='nav-item fade-in'><Link className='nav-link' to='/login' onClick={this.toggleCollapse}>Login</Link></li>
+                  <li className='nav-item fade-in'><Link className='nav-link' to='/register' onClick={this.toggleCollapse}>Register</Link></li>
                   </>}
             </ul>
           </div>
