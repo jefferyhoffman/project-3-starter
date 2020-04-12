25de62c4c1791cb3f33d58eb625e2bf037e0db8a
Adriana <adriana.j.didden@gmail.com>
2020-04-12 00:11:19 -0400

    save auth drop style

--- client/src/components/AuthDropdown/AuthDropdown.js
+++ client/src/components/AuthDropdown/AuthDropdown.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import withStyles from "@material-ui/styles/withStyles";
+
 // import { Link } from 'react-router-dom';
-import Gravatar from 'react-gravatar';
+// import Gravatar from 'react-gravatar';
+import { Button } from "@material-ui/core";
 
 
 import AuthContext from '../../contexts/AuthContext';
+
+const styles = (theme) => ({
+  actionButton: {
+    textTransform: "uppercase",
+    margin: theme.spacing(1),
+    width: 100,
+  },
+  alignRight: {
+    display: "flex",
+    justifyContent: "flex-end",
+  }
+
+
+});
+
 
 class AuthDropdown extends Component {
   static contextType = AuthContext;
@@ -24,24 +42,28 @@ class AuthDropdown extends Component {
   }
 
   render() {
+    const { classes } = this.props;
+
     const { user } = this.context;
     const { isOpen } = this.state;
 
     const dropdownMenuClass = `dropdown-menu dropdown-menu-right ${isOpen && 'show'}`;
 
     return (
-      <li className="nav-item dropdown">
-        <button className="btn btn-link dropdown-toggle" onClick={this.toggleOpen} id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <Gravatar className="rounded-circle" email={user.email} size={30} /> {user.email}
-        </button>
-        <div className={dropdownMenuClass} aria-labelledby="navbarDropdown">
-          <div className="dropdown-item" onClick={this.handleLogout}>Logout</div>
-        </div>
-      </li>
+      <div className={classes.alignRight}>
+        <Button className="btn btn-link dropdown-toggle" onClick={this.toggleOpen} id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          {/* <Gravatar className="rounded-circle" email={user.email} size={30} /> */}
+           {user.email}
+        </Button>
+          <Button color="primary"
+              variant="outlined"
+              size="small"
+              className={classes.actionButton} onClick={this.handleLogout}>Logout</Button>
+      </div>
     );
 
 
   }
 }
 
-export default AuthDropdown;
+export default withStyles(styles)(AuthDropdown);
