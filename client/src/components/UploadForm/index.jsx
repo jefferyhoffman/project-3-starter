e6abd35d4c31de4e90bbace1fedf0292e2885d24
Kevin Parmar <parmarkevinb@gmail.com>
2020-04-12 21:57:23 -0400

    Added transition to upload form

--- client/src/components/UploadForm/index.jsx
+++ client/src/components/UploadForm/index.jsx
@@ -23,12 +23,12 @@ class UploadForm extends React.Component {
   
     render() {
       return (
-        <div>
-          <h2 className='abel'>Upload a New Picture!</h2>
+        <div className='form-holder'>
+          <h2 className='abel translate-up'>Upload a New Picture!</h2>
           <div className='form' action="/api/img-upload" method="POST" >
             <input type="file" name="file" id="file" className="inputfile" onChange={this.handleInputChange}/>
-            <label className='modern-btn titillium'htmlFor="file">{this.state.filename}</label>
-            <button className='modern-btn titillium' onClick={this.handleSubmit}>Create!</button>
+            <label className='modern-btn titillium translate-up'htmlFor="file">{this.state.filename}</label>
+            <button className='modern-btn titillium translate-up' onClick={this.handleSubmit}>Create!</button>
           </div>
         </div>
       );
