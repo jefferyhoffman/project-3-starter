9159516e801f99a70b1c0321448382898f323b0b
Martin <melloncollie221@aol.com>
2020-04-13 13:49:04 -0400

    nav bar back to normal

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,67 +1,114 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "../Navigation/Navigation.css";
 import SiteLogo from "../Navigation/SiteLogo.png";
-import AuthContext from '../../contexts/AuthContext';
-import AuthDropdown from '../../components/AuthDropdown/AuthDropdown';
+import AuthContext from "../../contexts/AuthContext";
+import AuthDropdown from "../../components/AuthDropdown/AuthDropdown";
 
 class Navigation extends Component {
   static contextType = AuthContext;
 
   state = {
-    collapsed: true
-  }
+    collapsed: true,
+  };
 
   toggleCollapse = () => {
     this.setState({
-      collapsed: !this.state.collapsed
+      collapsed: !this.state.collapsed,
     });
-  }
+  };
 
   render() {
     const { user } = this.context;
     const { collapsed } = this.state;
-    const targetClass = `collapse navbar-collapse ${!collapsed && 'show'}`;
-    const togglerClass = `navbar-toggler ${collapsed && 'collapsed'}`;
+    const targetClass = `collapse navbar-collapse ${!collapsed && "show"}`;
+    const togglerClass = `navbar-toggler ${collapsed && "collapsed"}`;
 
     return (
-      <div> 
+      <div>
         <div className="Header">
-        <img src={SiteLogo} alt="The Cadenza logo" id="sitelogo"></img>
+          <img src={SiteLogo} alt="The Cadenza logo" id="sitelogo"></img>
         </div>
-      <div className='Navigation col-md-6 text-center'>
-        <nav className='navbar navbar-expand-lg navbar-dark bg-dark mb-3 col-md-6 text-center'>
-          {/* <Link className='navbar-brand' to='#'>Project 3</Link> */}
-          <button className={togglerClass} onClick={this.toggleCollapse} data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
-            <span className='navbar-toggler-icon'></span>
-          </button>
+        <div className="Navigation col-md-6 text-center">
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-3 col-md-6 text-center">
+            {/* <Link className='navbar-brand' to='#'>Project 3</Link> */}
+            <button
+              className={togglerClass}
+              onClick={this.toggleCollapse}
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
 
-          <div className={targetClass} id='navbarSupportedContent'>
-            <ul className='navbar-nav col-md-6 text-center'>
-              <li className='nav-item col-md-6 text-center'>
-                <Link className='nav-link' to='/' onClick={this.toggleCollapse}>Home</Link>
-              </li>
-              <li className='nav-item col-md-6 text-center'>
-                <Link className='nav-link' to='/thread' onClick={this.toggleCollapse}>Threads</Link>
-              </li>
-              {user &&
-                <li className='nav-item col-md-6 text-center'>
-                  <Link className='nav-link' to='/secret' onClick={this.toggleCollapse}>Secret</Link>
-                </li>}
-            </ul>
-            <ul className='navbar-nav mr-auto right col-md-6 text-center'>
-              <li className='nav-item col-md-6 text-center'>
-                <Link className='nav-link' to='/profile' onClick={this.toggleCollapse}>Profile</Link>
-              </li>
-            </ul>
-            <ul className='navbar-nav col-md-6 text-center'>
-              {user
-                ? <AuthDropdown onClick={this.toggleCollapse} />
-                : <li className='nav-item col-md-6 text-center'><Link className='nav-link' to='/login' onClick={this.toggleCollapse}>Login/Register</Link></li>}
-            </ul>
-          </div>
-        </nav>
-      </div>
+            <div
+              className={targetClass}
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    to="/"
+                    onClick={this.toggleCollapse}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    to="/thread"
+                    onClick={this.toggleCollapse}
+                  >
+                    Threads
+                  </Link>
+                </li>
+                {user && (
+                  <li className="nav-item">
+                    <Link
+                      className="nav-link"
+                      to="/secret"
+                      onClick={this.toggleCollapse}
+                    >
+                      Secret
+                    </Link>
+                  </li>
+                )}
+              </ul>
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    to="/profile"
+                    onClick={this.toggleCollapse}
+                  >
+                    Profile
+                  </Link>
+                </li>
+                {user ? (
+                  <AuthDropdown onClick={this.toggleCollapse} />
+                ) : (
+                    <li className="nav-item">
+                      <Link
+                        className="nav-link"
+                        to="/login"
+                        onClick={this.toggleCollapse}
+                      >
+                        Login/Register
+                    </Link>
+                    </li>
+                  )}
+              </ul>
+              {/* <ul className='navbar-nav mr-auto right col-md-6 text-center'>
+              
+            </ul> */}
+            </div>
+          </nav>
+        </div>
       </div>
     );
   }
