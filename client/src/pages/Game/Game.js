13bd925adca8ddd72b308589c04eff984ffa40d4
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-07-04 12:47:57 -0400

    adde place holder pages for Game and Scoreboard

---
+++ client/src/pages/Game/Game.js
@@ -0,0 +1,14 @@
+
+import logo from './logo.svg';
+
+class GamePage extends Component {
+  render() {
+    return (
+      <div className='Game'>
+        <hq>Game page</hq>
+      </div>
+    );
+  }
+}
+
+export default GamePage;
---
+++ client/src/pages/ScoreBoard/ScoreBoard.js
@@ -0,0 +1,14 @@
+
+import logo from './logo.svg';
+
+class ScoreBoardPage extends Component {
+  render() {
+    return (
+      <div className='ScoreBoard'>
+        <h1>ScoreBoard</h1>
+      </div>
+    );
+  }
+}
+
+export default ScoreBoardPage;
