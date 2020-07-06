7210e17f8c4bd0bd761dc00bd24e3e07a1e27c18
George Flores <georgeflores886@gmail.com>
2020-07-06 13:08:23 -0400

    refactored api routes for characters

--- server/controllers/api/api-routes.js
+++ server/controllers/api/api-routes.js
@@ -1,54 +1,55 @@
-const db = require('../models');
+const characterController = require('express').Router();
+const db = require('../../models');
 
-module.exports = function (app) {
-    app.get('/api/characters', (req, res)=> {
-        db.Character.find({})
+
+characterController.get('/api/characters', (req, res) => {
+    db.Character.find({})
         .then(allCharacters => {
             res.json(allCharacters);
         })
         .catch(err => {
-            res.json(err);
+            res.status(500).json(err);
         })
-    });
-    
-    //when the user interacts with card?
-    app.put('/api/characters/:id', (req, res)=> {
-        db.Character.updateOne(
-            
-        )
-    });
+});
 
-    //leaderboard routes
-    //get leaderboard
-    app.get('/api/leaderboard/', (req, res) => {
-        db.Leaderboard.find({})
+//when the user interacts with card?
+characterController.put('/api/characters/:id', (req, res) => {
+    db.Character.updateOne(
+
+    )
+});
+
+//leaderboard routes
+//get leaderboard
+characterController.get('/api/leaderboard/', (req, res) => {
+    db.Leaderboard.find({})
         .then(score => {
             res.json(score)
         })
         .catch(err => {
-            res.json(err);
+            res.status(500).json(err);
         })
-    });
-    //update leaderboard
-    app.put('/api/leaderboard/:id', (req, res) => {
-        db.Leaderboard.updateOne(
-        {_id: req.params.id}
-        );
-    });
-    
-    //user posting scores
-    app.post('/api/leaderboard/', (req, res) => {
-        let {userName, score} = req.body;
+});
+//update leaderboard
+characterController.put('/api/leaderboard/:id', (req, res) => {
+    db.Leaderboard.updateOne(
+        { _id: req.params.id }
+    );
+});
 
-        db.Leaderboard.insertOne({userName, score})
+//user posting scores
+characterController.post('/api/leaderboard/', (req, res) => {
+    let { userName, score } = req.body;
+
+    db.Leaderboard.insertOne({ userName, score })
         .then(userScore => {
             console.log("user score regiestered")
             res.json(userScore);
         })
-        .catch(err=> {
+        .catch(err => {
             res.json(err);
         })
-    });
-    
-}
+});
+
+module.exports = gameController
 
