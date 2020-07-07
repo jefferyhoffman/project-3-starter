6b92183e8c890d5b304aeb2b36ee1f142825d81f
Khamari Thompson <61212885+Khamari13@users.noreply.github.com>
2020-07-07 12:11:18 -0400

    updated login page

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,74 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react'
+import { Button, Form, Segment } from 'semantic-ui-react'
 
-import Octicon, { Mail, Key } from '@githubprimer/octicons-react';
-
-class LoginForm extends Component {
-  state = {
-    email: '',
-    password: ''
-  };
-
-  handleInputChange = event => {
-    const { name, value } = event.target;
-
-    this.setState({
-      [name]: value
-    });
-  }
-
-  handleSubmit = event => {
-    const { email, password } = this.state;
-
-    this.props.onSubmit(email, password);
-    event.preventDefault();
-  }
-
-  render() {
-    const { email, password } = this.state;
-
-    return (
-      <div className='LoginForm'>
-        <div className='card'>
-          <div className='card-body'>
-            <form className='LoginForm' onSubmit={this.handleSubmit}>
-              <div className='input-group mb-3'>
-                <div className="input-group-prepend">
-                  <span className="input-group-text"><Octicon icon={Mail} /></span>
-                </div>
-                <input
-                  className='form-control'
-                  id='email'
-                  type='email'
-                  name='email'
-                  placeholder='email@provider.com'
-                  value={email}
-                  onChange={this.handleInputChange}
-                />
-              </div>
-
-              <div className='input-group mb-3'>
-                <div className="input-group-prepend">
-                  <span className="input-group-text"><Octicon icon={Key} /></span>
-                </div>
-                <input
-                  className='form-control'
-                  id='password'
-                  type='password'
-                  name='password'
-                  placeholder='password'
-                  value={password}
-                  onChange={this.handleInputChange}
-                />
-              </div>
-
-              <button className='btn btn-primary' type='submit'>Login</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
-
-export default LoginForm;
+const LoginForm = () => (
+  <Segment inverted>
+    <Form inverted>
+      <Form.Group widths='equal'>
+        <Form.Input fluid label='First name' placeholder='First name' />
+        <Form.Input fluid label='Last name' placeholder='Last name' />
+        <Form.Input fluid label='E-mail' placeholder='E-mail' />
+      </Form.Group>
+      <Form.Checkbox label='I agree to the Terms and Conditions' />
+      <Button type='submit'>Submit</Button>
+    </Form>
+  </Segment>
+)
+export default LoginForm
 
