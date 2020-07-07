afc1164d93f2cb52ffa57a2f33eed634bb292400
Khamari Thompson <61212885+Khamari13@users.noreply.github.com>
2020-07-07 14:05:06 -0400

    added user name box to registration form

--- client/src/components/RegistrationForm/RegistrationForm.js
+++ client/src/components/RegistrationForm/RegistrationForm.js
@@ -4,12 +4,12 @@ import Octicon, { Mail, Key } from '@githubprimer/octicons-react';
 
 class RegistrationForm extends Component {
   state = {
-    firstName: '',
-    lastName: '',
-    userName: '',
-    email: '',
-    password: '',
-    passwordConfirm: ''
+    firstName: "",
+    lastName: "",
+    userName: "",
+    email: "",
+    password: "",
+    passwordConfirm: ""
   };
 
   handleInputChange = event => {
@@ -31,84 +31,98 @@ class RegistrationForm extends Component {
     const { firstName, lastName, userName, email, password, passwordConfirm } = this.state;
 
     return (
-      <div className='LoginForm'>
-        <div className='card'>
-          <div className='card-body'>
+      <div className="LoginForm">
+        <div className="card">
+          <div className="card-body">
             <form onSubmit={this.handleSubmit}>
-            <div className='input-group mb-3'>
+            <div className="input-group mb-3">
                 <div className="input-group-prepend">
                   <span className="input-group-text"><Octicon icon={Mail} /></span>
                 </div>
                 <input
-                  className='form-control'
-                  id='first-name'
-                  type='text'
-                  name='firstName'
-                  placeholder='First Name'
+                  className="form-control"
+                  id="first-name"
+                  type="text"
+                  name="firstName"
+                  placeholder="First Name"
                   value={firstName}
                   onChange={this.handleInputChange}
                 />
               </div>
-              <div className='input-group mb-3'>
+              <div className="input-group mb-3">
                 <div className="input-group-prepend">
                   <span className="input-group-text"><Octicon icon={Mail} /></span>
                 </div>
                 <input
-                  className='form-control'
-                  id='last-name'
-                  type='text'
-                  name='lastName'
-                  placeholder='Last Name'
+                  className="form-control"
+                  id="last-name"
+                  type="text"
+                  name="lastName"
+                  placeholder="Last Name"
                   value={lastName}
                   onChange={this.handleInputChange}
                 />
               </div>
-              <div className='input-group mb-3'>
+              <div className="input-group mb-3">
                 <div className="input-group-prepend">
                   <span className="input-group-text"><Octicon icon={Mail} /></span>
                 </div>
                 <input
-                  className='form-control'
-                  id='email'
-                  type='email'
-                  name='email'
-                  placeholder='email@provider.com'
+                  className="form-control"
+                  id="user-name"
+                  type="text"
+                  name="userName"
+                  placeholder="User Name"
+                  value={userName}
+                  onChange={this.handleInputChange}
+                />
+              </div>
+              <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                  <span className="input-group-text"><Octicon icon={Mail} /></span>
+                </div>
+                <input
+                  className="form-control"
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="email@provider.com"
                   value={email}
                   onChange={this.handleInputChange}
                 />
               </div>
 
-              <div className='input-group mb-3'>
+              <div className="input-group mb-3">
                 <div className="input-group-prepend">
                   <span className="input-group-text"><Octicon icon={Key} /></span>
                 </div>
                 <input
-                  className='form-control'
-                  id='password'
-                  type='password'
-                  name='password'
-                  placeholder='password'
+                  className="form-control"
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="password"
                   value={password}
                   onChange={this.handleInputChange}
                 />
               </div>
 
-              <div className='input-group mb-3'>
+              <div className="input-group mb-3">
                 <div className="input-group-prepend">
                   <span className="input-group-text"><Octicon icon={Key} /></span>
                 </div>
                 <input
-                  className='form-control'
-                  id='password-confirm'
-                  type='password'
-                  name='passwordConfirm'
-                  placeholder='password (again)'
+                  className="form-control"
+                  id="password-confirm"
+                  type="password"
+                  name="passwordConfirm"
+                  placeholder="password (again)"
                   value={passwordConfirm}
                   onChange={this.handleInputChange}
                 />
               </div>
 
-              <button className='btn btn-primary' type='submit'>Register Now!</button>
+              <button className="btn btn-primary" type="submit">Register Now!</button>
             </form>
           </div>
         </div>
