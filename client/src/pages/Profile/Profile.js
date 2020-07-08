2c1889e1524f9ab5c00ab5838bef4530800216f4
Thomas Rider <thomasjrideriii@gmail.com>
2020-07-08 10:35:08 -0400

    working on profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,41 +1,49 @@
 import React, { Component } from "react";
-import { Card, Icon, Image, Container } from "semantic-ui-react";
+import { Card, Rating, Icon, Image, Container, Grid } from "semantic-ui-react";
 
 import Gravatar from "react-gravatar";
 import AuthContext from "../../contexts/AuthContext";
 
+const extra = <Rating icon="star" defaultRating={3} maxRating={5} />;
+
+
 class Profile extends Component {
     static contextType = AuthContext;
 
-    state ={
-
-    }
+    state = {};
 
     render() {
         const { user } = this.context;
 
-        return(
+        return (
             <Container>
-                <Card fluid>
-                  <Gravatar className="ui medium centered image" email="thomasjrideriii@gmail.com" size="200"/>
-                  <Card.Content>
-                    <Card.Header>Matthew</Card.Header>
-                    <Card.Meta>
-                      <span className='date'>Joined in 2015</span>
-                    </Card.Meta>
-                    <Card.Description>
-                      Matthew is a musician living in Nashville.
-                    </Card.Description>
-                  </Card.Content>
-                  <Card.Content extra>
-                    <a>
-                      <Icon name='user' />
-                      22 Friends
-                    </a>
-                  </Card.Content>
+                <Card fluid width={4}>
+                    <Gravatar
+                        className="ui medium centered image"
+                        email="thomasjrideriii@gmail.com"
+                        size="200"
+                    />
+                    <Card.Content>
+                        <Card.Header>Matthew</Card.Header>
+                        <Card.Meta>
+                            <span className="date">Joined in 2015</span>
+                        </Card.Meta>
+                        <Card.Description>
+                            Matthew is a musician living in Nashville.
+                        </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                        <a>
+                            <Icon name="user" />
+                            22 Friends
+                        </a>
+                    </Card.Content>
                 </Card>
+                <Card.Group fluid width={8}>
+                  
+                </Card.Group>
             </Container>
-        )
+        );
     }
 }
 
