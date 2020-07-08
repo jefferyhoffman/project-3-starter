477747c0c7184eed30dc5d14419b2033c8c780c4
Thomas Rider <thomasjrideriii@gmail.com>
2020-07-08 10:04:59 -0400

    added basic card framework for profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Card, Icon, Image, Container } from "semantic-ui-react";
 
 import Gravatar from "react-gravatar";
 import AuthContext from "../../contexts/AuthContext";
@@ -14,10 +15,26 @@ class Profile extends Component {
         const { user } = this.context;
 
         return(
-            <div>
-                <h1>This should be the user data</h1>
-                <p>{user}</p>
-            </div>
+            <Container>
+                <Card fluid>
+                  <Gravatar className="ui medium centered image" email="thomasjrideriii@gmail.com" size="200"/>
+                  <Card.Content>
+                    <Card.Header>Matthew</Card.Header>
+                    <Card.Meta>
+                      <span className='date'>Joined in 2015</span>
+                    </Card.Meta>
+                    <Card.Description>
+                      Matthew is a musician living in Nashville.
+                    </Card.Description>
+                  </Card.Content>
+                  <Card.Content extra>
+                    <a>
+                      <Icon name='user' />
+                      22 Friends
+                    </a>
+                  </Card.Content>
+                </Card>
+            </Container>
         )
     }
 }
