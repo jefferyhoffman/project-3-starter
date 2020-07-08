dcfc81374fa97f0d72de842973a13914d337360a
Andrew Greer <62513150+andrewpaulgreer@users.noreply.github.com>
2020-07-08 18:02:12 -0400

    added logic for the dropdown

--- client/src/components/DropDown/DropDown.js
+++ client/src/components/DropDown/DropDown.js
@@ -1,152 +1,132 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 
 const questions = [
     {
-    text:"Blue Hair?", value: "blue"
+    text:"Hair Color?"
     },
     {
-    text:"Red Hair?" , value: "red"
+    text:"Eye Color?"
     },
-    // {
-    // text:"Brown Hair?", value: "brown"
-    // },
-    // {
-    // text:"Gray Hair?", value: "gray"
-    // },
-    // {
-    // text:"Black Hair?", value: "black"
-    // },
-    // {
-    // text:"Wear Glasses?", value: true
-    // },
-    // {
-    // text:"No Glasses?", value: false
-    // },
-    // {
-    // text:"Have Facial Hair?", value: true
-    // },
-    // {
-    // text:"No Facial Hair?", value: false
-    // },
-    // {
-    // text:"Blue Eyes?", value: "blue"
-    // },
-    // {
-    // text:"Hazel Eyes?", value: "hazel"
-    // },
-    // {
-    // text:"Brown Eyes?", value: "brown"
-    // },
-    // {
-    // text:"Green Eyes?", value: "green"
-    // },
-    // {
-    // text:"found $10,000 in a couch?", value: "Found 10 grand in a couch."
-    // },
+    {
+    text:"Wears Glasses?"
+    },
+    {
+    text:"Has Facial Hair?"
+    },
+    {
+    text:"Cool Fact?"
+    },
     ]
 
 
- const DropDown = (props) => {
-     const [choice, setchoice]= useState(props.choice)
+//  const DropDown = (props) => {
+//      const [choice, setchoice]= useState(props.choice)
 
-     return(
-        <div className="column is-one-quarter">
-                        <div className="dropdown is-hoverable">
-                            <div className="dropdown-trigger">
-                                <button className="button dropdown-button is-rounded" aria-haspopup="true" aria-controls="dropdown-menu">
-                                    <span> Choose Your Question! </span>
-                                </button>
-                            </div>
-                            <div className="dropdown-menu" id="dropdown-menu" role="menu">
-                                <div className="dropdown-content">
-                                    {questions.map((questiontext, value) => 
-                                     <button className="dropdown-item" onClick={e=> console.log(choice)} value="blue">{questiontext.text}</button>
-                                    )}
+//      return(
+//         <div className="column is-one-quarter">
+//                         <div className="dropdown is-hoverable">
+//                             <div className="dropdown-trigger">
+//                                 <button className="button dropdown-button is-rounded" aria-haspopup="true" aria-controls="dropdown-menu">
+//                                     <span> Choose Your Question! </span>
+//                                 </button>
+//                             </div>
+//                             <div className="dropdown-menu" id="dropdown-menu" role="menu">
+//                                 <div className="dropdown-content">
+//                                     {questions.map((questiontext, value) => 
+//                                      <button className="dropdown-item" onClick={e=> console.log(choice)} value="blue">{questiontext.text}</button>
+//                                     )}
         
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-     )
- }
- 
-
-// class DropDown extends Component {
-//     constructor(){
-//         super();
-//         this.state={
-//         hairQ:"",
-//         glassesQ: "",
-//         facialhairQ: "",
-//         eyeQ: "",
-//         factQ: "",
-//         qChoice: "",
-//         }
-//     }
-    
-//     componentDidMount(){
-//         axios.get('/api/characters')
-//         .then((res)=>{
-//             this.setState({qChoice: Math.floor(Math.random()*9)})
-//           this.setState({hairQ: res.data[this.state.qChoice].hairColor})
-//           this.setState({glassesQ: res.data[this.state.qChoice].glasses})
-//           this.setState({facialhairQ:res.data.facialHair})
-//           this.setState({eyeQ:res.data.eyeColor})
-//           this.setState({factQ: res.data.fact}) 
-//           console.log(res.data)
-//           console.log(this.state.hairQ)
-//           console.log(this.state.glassesQ)
-    
-//         })
-//     }
-
-//     handleAnswer(){
-//         axios.get('/api/characters')
-//         .then((res)=>{
-//         if (questions[this.state.qChoice].value && this.state.hairQ !== res.data.hairColor){
-//             console.log(questions.value)
-//             alert("incorrect")
-//         } else if (questions.value && this.state.glassesQ !== res.data.glasses){
-//             alert("incorrect")
-//         } else if (questions.value && this.state.facialhairQ !== res.data.facialHair){
-//             alert("incorrect")
-//         }else if (questions.value && this.state.eyeQ !== res.data.eyeColor){
-//             alert("incorrect")
-//         }else if (questions.value && this.state.factQ !== res.data.fact){
-//             alert("incorrect")
-//         }
-            
-//     })
-//     }
-    
-
-
-
-//     render() {
-//         return (
-            
-//             <div className="column is-one-quarter">
-//                 <div className="dropdown is-hoverable">
-//                     <div className="dropdown-trigger">
-//                         <button className="button dropdown-button is-rounded" aria-haspopup="true" aria-controls="dropdown-menu">
-//                             <span> Choose Your Question! </span>
-//                         </button>
-//                     </div>
-//                     <div className="dropdown-menu" id="dropdown-menu" role="menu">
-//                         <div className="dropdown-content">
-//                             {questions.map((questiontext, value) => 
-//                              <button className="dropdown-item" onClick={this.handleAnswer}>{questiontext.text}</button>
-//                             )}
-
+//                                 </div>
+//                             </div>
 //                         </div>
 //                     </div>
-//                 </div>
-//             </div>
-//         )
-//     }
+//      )
+//  }
+ 
 
-// }
+const DropDown = (props) => {
+   
+    const [choice, setchoice]= useState(props.choice)
+    const [hair, setHair] = useState("")
+    const [eye, setEye] = useState("")
+    const [glassesQ, setGlassesQ] = useState("")
+    const [face, setFace] = useState("")
+    const [factQ, setFactQ] = useState("")
+
+    useEffect(()=> {
+        axios.get('/api/characters')
+        .then((res) => {
+           setHair(res.data[choice].hairColor)
+           setEye(res.data[choice].eyeColor)
+           setGlassesQ(res.data[choice].glasses)
+           setFace(res.data[choice].facialHair)
+           setFactQ(res.data[choice].fact)
+           
+        }).then(()=> {
+            console.log(hair) 
+            console.log(eye)
+            console.log(glassesQ)
+            console.log(face)
+            console.log(factQ)
+            
+        })
+    }, [])
+    
+   function handleAnswer () {
+       console.log("here")
+       
+        if (questions[0].text){
+            console.log(hair)
+            alert(`Hair color is ${hair}`)
+        } else if (questions[1].text){
+            console.log(eye)
+            alert(`Eye color is ${eye}`)
+        } else if(questions[2].text){
+            console.log(glassesQ)
+            alert(`Wears Glasses:  ${glassesQ}`)
+        }else if (questions[3].text){
+            console.log(face)
+            alert(`Has Facial Hair: ${face}`)
+        }else if (questions[4].text){
+            console.log(factQ)
+            alert(`Cool Fact: ${factQ}`)
+        } else {
+            alert("please choose a question")
+        }
+            
+    }
+
+ function handleEye(){
+    if (questions[1].text){
+        console.log(eye)
+        alert(`Eye color is ${eye}`)
+    }
+ }
+
+
+    return (
+            <div className="column is-one-quarter">
+                <div className="dropdown is-hoverable">
+                    <div className="dropdown-trigger">
+                        <button className="button dropdown-button is-rounded" aria-haspopup="true" aria-controls="dropdown-menu">
+                            <span> Choose Your Question! </span>
+                        </button>
+                    </div>
+                    <div className="dropdown-menu" id="dropdown-menu" role="menu">
+                        <div className="dropdown-content">
+                            {questions.map((question) => 
+                             <button className="dropdown-item" 
+                             onClick={handleAnswer} 
+                             value="blue">{question.text}</button>
+                            
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+}
 
 export default DropDown
