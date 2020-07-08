12039b39b9a003b34cfb7554f69b8c71bbab40de
George Flores <georgeflores886@gmail.com>
2020-07-08 14:52:33 -0400

    rendered character cards in characterpage

--- client/src/pages/Characters/Characters.js
+++ client/src/pages/Characters/Characters.js
@@ -2,44 +2,19 @@ import React, { useEffect, useState, Component } from 'react'
 import ReactCardFlip from 'react-card-flip';
 import axios from 'axios'
 
-
 const Characters = () => {
-    const [character, setCharacter] = useState({
-        isFlipped: false,
-        name: "",
-        hairColor: "",
-        glasses: true,
-        facialHair: true ,
-        eyeColor: "",
-        fact: "",
-        story:""
-    })
+    const [character, setCharacter] = useState([]);
 
     //handle card flip
     const handleFlip = () => {
         setCharacter(prevState => ({ isFlipped: !prevState.isFlipped }));
     };
-
-    useEffect(() =>{
-       axios.get('/api/characters')
+    useEffect(() => {
+        axios.get('/api/characters')
             .then((res) => {
-                setCharacter({ name: res.data.name })
-                setCharacter({ hairColor: res.data.hairColor })
-                setCharacter({ glasses: res.data.glasses })
-                setCharacter({ facialHair: res.data.facialHair })
-                setCharacter({ eyeColor: res.data.eyeColor })
-                setCharacter({ fact: res.data.fact})
-                setCharacter({story: res.data.story})
-            }) 
+                setCharacter(res.data)
+            })
     }, []);
-        
-    const handleInputChange = event => {
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value
-        });
-    };
-
 
     return (
         <div className="container">
@@ -47,14 +22,31 @@ const Characters = () => {
                 <h1 className="is-size-1">😻Characters😻</h1>
             </div>
             <br />
-            <div className="columns is-centered">
-                <div className="tile is-ancestor">
 
-                    {/* Mapping through characters to render api */}
 
-                    {/* End of map */}
-                </div>
+            <div className="columns is-centered is-multiline">
+
+                {/* Mapping through characters to render api */}
+                {character.map((char) => {
+                    return (
+                        <div className="column is-one-quarter">
+                            <div className="box" style={{ maxHeight: "400px" }}>
+                                <h4> {char.name}</h4>
+                                <img alt={char.name} src={char.picture} style={{ width: "200px", height: "200px" }} />
+
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type 
+                                and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, 
+                                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and
+                                more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                
+                            </div>
+                        </div>
+                    )
+                })}
+                {/* End of map */}
             </div>
+
         </div>
     )
 
