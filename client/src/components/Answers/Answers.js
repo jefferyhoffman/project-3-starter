35f5e00408df33c05c152e04f31cc8166f31b5fe
Lindsey Lawson <lynseahoss@gmail.com>
2020-07-08 00:04:33 -0400

    updated character answer card

--- client/src/components/Answers/Answers.js
+++ client/src/components/Answers/Answers.js
@@ -1,24 +1,31 @@
-import React, { Component } from 'react'
-import './Answers.css'
-class Answers extends Component{
-state ={
-    score:10
-}
-    render(){
-        return(
-            
-            <div className="column is-4 is-offset-8">
-                <div class="tile is-ancestor">
-                <div className="tile is-parent">
-            <article className="tile is-child box scoreBox2">
-            <article className="tile is-child box">
-                <h1 className="is-size-5 has-text-centered" > <strong>Character Answers: {this.state.score}</strong></h1>
-                </article>
+iimport React, { Component } from "react";
+import "./Answers.css";
+class Answers extends Component {
+  state = {
+    score: 10
+  };
+  render() {
+    return (
+      <div className="column is-4">
+        <div class="tile is-ancestor">
+          <div className="tile is-parent">
+            <article className="tile is-child box answerBox">
+              <div class="card">
+                <div class="card-content">
+                  <p className="title">Character Facts</p>
+                  <ul>
+                    <li>1. Character Wears Glasses</li>
+                    <li>2. Character has blue eyes</li>
+                    <li>3. Character is obessed with phone</li>
+                    <li>4. Character made it big on TikTok</li>
+                  </ul>
+                </div>
+              </div>
             </article>
-            </div>
-            </div>
-            </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
- export default Answers;
+export default Answers;
