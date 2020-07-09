28f70d8dad32a3e00c90c39d84773f27c2b045b1
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-07-09 14:47:35 -0400

    Files to seed db

---
+++ server/seeders/user-data.sql
@@ -0,0 +1,9 @@
+INSERT INTO recipes_db.users (email, password, username, firstName, lastName, createdAt, updatedAt)
+VALUES ("jhoffman@trilogyed.com", "test", "daBoss", "Jeff", "Hoffman", "2020-07-08", "2020-07-08"),
+("maxA@trilogyed.com", "test", "daHelp", "Max", "Allee", "2020-07-08", "2020-07-08"),
+("caleb@trilogyed.com", "test", "daNewGuy", "Caleb", "Crum", "2020-07-08", "2020-07-08"),
+("heather@test.com", "test", "lone-girl", "Heather", "Sorrells", "2020-07-08", "2020-07-08"),
+("jesse@test.com", "test", "serverBoss", "Jesse", "VanSlyke", "2020-07-08", "2020-07-08"),
+("ben@test.com", "test", "kamakazi-Ben", "Ben", "Cart", "2020-07-08", "2020-07-08"),
+("khamari@test.com", "test", "K-Man", "Khamari", "Thompson", "2020-07-08", "2020-07-08"),
+("thomas@test.com", "test", "obiwan", "Thomas", "Rider", "2020-07-08", "2020-07-08");
