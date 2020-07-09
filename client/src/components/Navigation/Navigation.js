5699b35ebf5b287fb842fa0db40f33fcfe916e53
Thomas Rider <thomasjrideriii@gmail.com>
2020-07-09 11:27:23 -0400

    Added navbar css file

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import {Menu, Button, Container, MenuHeader} from 'semantic-ui-react';
-import { NavLink, withRouter } from 'react-router-dom';
+import {Menu, Button, Container } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 import AuthContext from '../../contexts/AuthContext';
 import AuthDropdown from '../../components/AuthDropdown/AuthDropdown';
+import "./Navigation.css"
 
 class Navigation extends Component {
   static contextType = AuthContext;
@@ -20,38 +21,9 @@ class Navigation extends Component {
 
   render() {
     const { user } = this.context;
-    const { collapsed } = this.state;
-    const targetClass = `collapse navbar-collapse ${!collapsed && 'show'}`;
-    const togglerClass = `navbar-toggler ${collapsed && 'collapsed'}`;
 
     return (
-      // <div className='Navigation'>
-      //   <nav className='navbar navbar-expand-lg navbar-light bg-light mb-3'>
-      //     <Link className='navbar-brand' to='#'>Project 3</Link>
-      //     <button className={togglerClass} onClick={this.toggleCollapse} data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
-      //       <span className='navbar-toggler-icon'></span>
-      //     </button>
-
-      //     <div className={targetClass} id='navbarSupportedContent'>
-      //       <ul className='navbar-nav mr-auto'>
-      //         <li className='nav-item'>
-      //           <Link className='nav-link' to='/' onClick={this.toggleCollapse}>Home</Link>
-      //         </li>
-      //         {user &&
-      //           <li className='nav-item'>
-      //             <Link className='nav-link' to='/secret' onClick={this.toggleCollapse}>Secret</Link>
-      //           </li>}
-      //       </ul>
-      //       <ul className='navbar-nav'>
-      //         {user
-      //           ? <AuthDropdown onClick={this.toggleCollapse} />
-      //           : <li className='nav-item'><Link className='nav-link' to='/login' onClick={this.toggleCollapse}>Login/Register</Link></li>}
-      //       </ul>
-      //     </div>
-      //   </nav>
-      // </div>
-
-      <Menu inverted={true} size="massive">
+      <Menu inverted={true} size="massive" className="navbar" style={{borderRadius: 0}}>
               <Container>
                 <Menu.Item as={NavLink} to="/" active>
                   Home
