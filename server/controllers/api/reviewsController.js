f14c0dda88b3893fe04ff01118a8d7c89baee151
Jesse VanSlyke <jesse.j.vanslyke@gmail.com>
2020-07-10 12:59:01 -0400

    Updaed GET route to only pull all reviews based on RecipeId

--- server/controllers/api/reviewsController.js
+++ server/controllers/api/reviewsController.js
@@ -8,8 +8,6 @@ const jwt = require("jsonwebtoken");
 reviewsController.get("/all/:id", (req, res) => {
     const RecipeId = req.params.id;
 
-    console.log(req.body);
-    console.log("RecipeId: ", RecipeId);
     db.Review.findAll({where: { RecipeId }}, {include: [{model: db.User, as: "users"}, {model: db.Recipe, as: "recipes"}]})
     .then(review => res.json(review))
     .catch(err => res.json(err));
