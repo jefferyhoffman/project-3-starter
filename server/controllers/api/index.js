646f112b6ce6706e471e24447b8f24a2a7fb98e0
Jesse VanSlyke <jesse.j.vanslyke@gmail.com>
2020-07-11 15:15:48 -0400

    Base categories API calls created for GET all categories and POST to create a new category

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -4,5 +4,6 @@ apiControllers.use("/users", require("./usersController"));
 apiControllers.use("/recipes", require("./recipesController"));
 apiControllers.use("/reviews", require("./reviewsController"));
 apiControllers.use("/ingredients", require("./ingredientsController"));
+apiControllers.use("/categories", require("./categoriesController"));
 
 module.exports = apiControllers;
