15da350ea45971924d8015c6debc871df27b6d02
Jesse VanSlyke <jesse.j.vanslyke@gmail.com>
2020-07-11 17:10:44 -0400

    Pushing recipes SQL data seeder

---
+++ server/seeders/recipes-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO recipes_db.categories
+VALUES ("Casual Dining"), ("Entree"), ("Breakfast"), ("Lunch"), ("Dinner"), ("Crock-Pot"), 
+("Dessert"), ("Cookies"), ("Syrup"), ("Non-Chick Food"), ("Italian"), ("Mexican"), ("Pasta"), ("Side Dish"), 
+("Vegetable Dish"), ("Chili"), ("Easy Recipes"), ("Fancy Fixin's")
