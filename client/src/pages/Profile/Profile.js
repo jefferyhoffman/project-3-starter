b092862dab5509ac93121a87724cefd93490c2cb
Thomas Rider <thomasjrideriii@gmail.com>
2020-07-12 16:54:46 -0400

    Restructured profile to render recipe cards dynamically

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,234 +1,85 @@
 import React, { Component } from "react";
 import {
-    Card,
-    Rating,
-    Icon,
-    Image,
-    Container,
-    Grid,
-    Sticky,
+  Card,
+//   Rating,
+//   Icon,
+//   Image,
+  Container,
+  Grid,
+  Sticky,
 } from "semantic-ui-react";
+import API from "../../lib/API"
+import RecipeCard from "../../components/RecipeCard/RecipeCard"
 
 import Gravatar from "react-gravatar";
 import AuthContext from "../../contexts/AuthContext";
 
-const extra = <Rating icon="star" defaultRating={3} maxRating={5} />;
 
 class Profile extends Component {
-    static contextType = AuthContext;
+  static contextType = AuthContext;
 
-    state = {
-        user: {},
-    };
+  state = {
+    recipes: [],
+    err: ""
+  };
 
-    render() {
-        const { user } = this.context;
 
-        return (
-            <Container>
-                <Grid columns={2}>
-                    <Grid.Row>
-                        <Grid.Column width={8}>
-                            <Sticky context={this.contextRef}>
-                                <Card fluid>
-                                    <Gravatar
-                                        className="ui medium centered image"
-                                        email="thomasjrideriii@gmail.com"
-                                        size="200"
-                                    />
-                                    <Card.Content>
-                                        <Card.Header>Matthew</Card.Header>
-                                        <Card.Meta>
-                                            <span className="date">
-                                                Joined in 2015
-                                            </span>
-                                        </Card.Meta>
-                                        <Card.Description>
-                                            Matthew is a musician living in
-                                            Nashville.
-                                        </Card.Description>
-                                    </Card.Content>
-                                    <Card.Content extra>
-                                        <a>
-                                            <Icon name="user" />
-                                            22 Friends
-                                        </a>
-                                    </Card.Content>
-                                </Card>
-                            </Sticky>
-                        </Grid.Column>
+//   componentDidMount() {
+//     API.Recipes.all()
+//     .then(response => {
+//       this.setState({recipes: response.data, err: ""})
+//     })
+//     .catch(err => this.setState({ err: err.message }))
+//   };
+  
 
-                        <Grid.Column width={8}>
-                            <Card.Group itemsPerRow={2}>
-                                <Card>
-                                    <Image
-                                        src={require("../../assets/images/burger1.jpg")}
-                                        wrapped
-                                        ui={false}
-                                        as="a"
-                                        href="https://www.thewholesomedish.com/the-best-classic-burger/"
-                                        target="_blank"
-                                    />
-                                    <Card.Content>
-                                        <Card.Header>Tasty Burger!</Card.Header>
-                                        <Card.Meta>
-                                            <span>Entree</span>
-                                        </Card.Meta>
-                                        <Card.Description>
-                                            This is one tasty burger. It is a
-                                            heart attack in a bun! Easy to make
-                                            with a relatively small list of
-                                            ingredients. This is guaranteed to
-                                            be a crowd pleaser!
-                                        </Card.Description>
-                                        <Card.Meta>
-                                            <span>Submitted by: UserName</span>
-                                        </Card.Meta>
-                                    </Card.Content>
-                                    <Card.Content>{extra}</Card.Content>
-                                </Card>
-                                <Card>
-                                    <Image
-                                        src={require("../../assets/images/burger1.jpg")}
-                                        wrapped
-                                        ui={false}
-                                        as="a"
-                                        href="https://www.thewholesomedish.com/the-best-classic-burger/"
-                                        target="_blank"
-                                    />
-                                    <Card.Content>
-                                        <Card.Header>Tasty Burger!</Card.Header>
-                                        <Card.Meta>
-                                            <span>Entree</span>
-                                        </Card.Meta>
-                                        <Card.Description>
-                                            This is one tasty burger. It is a
-                                            heart attack in a bun! Easy to make
-                                            with a relatively small list of
-                                            ingredients. This is guaranteed to
-                                            be a crowd pleaser!
-                                        </Card.Description>
-                                        <Card.Meta>
-                                            <span>Submitted by: UserName</span>
-                                        </Card.Meta>
-                                    </Card.Content>
-                                    <Card.Content>{extra}</Card.Content>
-                                </Card>
-                                <Card>
-                                    <Image
-                                        src={require("../../assets/images/burger1.jpg")}
-                                        wrapped
-                                        ui={false}
-                                        as="a"
-                                        href="https://www.thewholesomedish.com/the-best-classic-burger/"
-                                        target="_blank"
-                                    />
-                                    <Card.Content>
-                                        <Card.Header>Tasty Burger!</Card.Header>
-                                        <Card.Meta>
-                                            <span>Entree</span>
-                                        </Card.Meta>
-                                        <Card.Description>
-                                            This is one tasty burger. It is a
-                                            heart attack in a bun! Easy to make
-                                            with a relatively small list of
-                                            ingredients. This is guaranteed to
-                                            be a crowd pleaser!
-                                        </Card.Description>
-                                        <Card.Meta>
-                                            <span>Submitted by: UserName</span>
-                                        </Card.Meta>
-                                    </Card.Content>
-                                    <Card.Content>{extra}</Card.Content>
-                                </Card>
-                                <Card>
-                                    <Image
-                                        src={require("../../assets/images/burger1.jpg")}
-                                        wrapped
-                                        ui={false}
-                                        as="a"
-                                        href="https://www.thewholesomedish.com/the-best-classic-burger/"
-                                        target="_blank"
-                                    />
-                                    <Card.Content>
-                                        <Card.Header>Tasty Burger!</Card.Header>
-                                        <Card.Meta>
-                                            <span>Entree</span>
-                                        </Card.Meta>
-                                        <Card.Description>
-                                            This is one tasty burger. It is a
-                                            heart attack in a bun! Easy to make
-                                            with a relatively small list of
-                                            ingredients. This is guaranteed to
-                                            be a crowd pleaser!
-                                        </Card.Description>
-                                        <Card.Meta>
-                                            <span>Submitted by: UserName</span>
-                                        </Card.Meta>
-                                    </Card.Content>
-                                    <Card.Content>{extra}</Card.Content>
-                                </Card>
-                                <Card>
-                                    <Image
-                                        src={require("../../assets/images/burger1.jpg")}
-                                        wrapped
-                                        ui={false}
-                                        as="a"
-                                        href="https://www.thewholesomedish.com/the-best-classic-burger/"
-                                        target="_blank"
-                                    />
-                                    <Card.Content>
-                                        <Card.Header>Tasty Burger!</Card.Header>
-                                        <Card.Meta>
-                                            <span>Entree</span>
-                                        </Card.Meta>
-                                        <Card.Description>
-                                            This is one tasty burger. It is a
-                                            heart attack in a bun! Easy to make
-                                            with a relatively small list of
-                                            ingredients. This is guaranteed to
-                                            be a crowd pleaser!
-                                        </Card.Description>
-                                        <Card.Meta>
-                                            <span>Submitted by: UserName</span>
-                                        </Card.Meta>
-                                    </Card.Content>
-                                    <Card.Content>{extra}</Card.Content>
-                                </Card>
-                                <Card>
-                                    <Image
-                                        src={require("../../assets/images/burger1.jpg")}
-                                        wrapped
-                                        ui={false}
-                                        as="a"
-                                        href="https://www.thewholesomedish.com/the-best-classic-burger/"
-                                        target="_blank"
-                                    />
-                                    <Card.Content>
-                                        <Card.Header>Tasty Burger!</Card.Header>
-                                        <Card.Meta>
-                                            <span>Entree</span>
-                                        </Card.Meta>
-                                        <Card.Description>
-                                            This is one tasty burger. It is a
-                                            heart attack in a bun! Easy to make
-                                            with a relatively small list of
-                                            ingredients. This is guaranteed to
-                                            be a crowd pleaser!
-                                        </Card.Description>
-                                        <Card.Meta>
-                                            <span>Submitted by: UserName</span>
-                                        </Card.Meta>
-                                    </Card.Content>
-                                    <Card.Content>{extra}</Card.Content>
-                                </Card>
-                            </Card.Group>
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </Container>
-        );
-    }
+
+  render() {
+    const { user } = this.context;
+    console.log(user)
+
+    return (
+      <Container>
+        <Grid columns={2}>
+          <Grid.Row>
+            <Grid.Column width={8}>
+                <Card fluid>
+                  <Gravatar
+                    className="ui medium centered image"
+                    email="thomas@test.com"
+                    size={200}
+                  />
+                  <Card.Content>
+                    <Card.Header>GARBAGE DATA</Card.Header>
+                    <Card.Meta>
+                      User number ###
+                    </Card.Meta>
+                    <Card.Description>
+                      Matthew is a musician living in Nashville.
+                    </Card.Description>
+                  </Card.Content>
+                </Card>
+            </Grid.Column>
+
+            <Grid.Column width={8}>
+              <Card.Group itemsPerRow={2}>
+                {this.state.recipes.map((recipe) => (
+                  <RecipeCard
+                    key={recipe.id}
+                    title={recipe.title}
+                    categories={recipe.categories}
+                    createdBy={recipe.createdBy}
+                    description={recipe.description}
+                    image={recipe.image}
+                  />
+                ))}
+              </Card.Group>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    );
+  }
 }
 
 export default Profile;
