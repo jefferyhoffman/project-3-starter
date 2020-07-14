c481379cc08d7627f25961450511419037074297
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-07-14 15:00:28 -0400

    Added delete button to recipe cards on user's profile page

--- client/src/components/ProfileRecipes/ProfileRecipes.js
+++ client/src/components/ProfileRecipes/ProfileRecipes.js
@@ -1,45 +1,66 @@
-import React from "react";
-import { Card, Rating, Image, Responsive, Container } from "semantic-ui-react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Card, Rating, Image, Responsive, Container, Button } from "semantic-ui-react";
+import AuthContext from '../../contexts/AuthContext';
+import API from '../../lib/API';
 
 // const extra = <Rating icon="star" defaultRating={3} maxRating={5} />;
 
-const ProfileRecipes = (props) => {
-  console.log(props);
-  const recipes = props.recipes.map((recipe) => {
-    let link = "/recipe/" + recipe.id
+class ProfileRecipes extends Component {
+  static contextType = AuthContext;
+
+  handleDelete(id) {
+    console.log(id);
+    const { authToken } = this.context;
+    API.Recipes.delete(id, authToken)
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((err) => console.log(err));
+        
+  }
+
+  render() {
+    console.log(this.props);
+    const recipes = this.props.recipes.map((recipe) => {
+      let link = "/recipe/" + recipe.id
+
+      return (
+        <Card key={recipe.id}>
+          <Image
+            as={Link} to={link}
+            src={recipe.image}
+            wrapped
+            ui={false}
+            target="_blank"
+          />
+          <Card.Content as={Link} to={link}>
+            <Card.Header>{recipe.title}</Card.Header>
+            <Card.Meta>
+              <span>
+                {recipe.categories.map((category) => category.category)}
+              </span>
+            </Card.Meta>
+            <Card.Description>{recipe.description}</Card.Description>
+            <Card.Meta>
+              <span>Submitted by: {recipe.createdBy}</span>
+            </Card.Meta>
+          </Card.Content>
+          <Button onClick={() => this.handleDelete(recipe.id)} className="delete">Delete</Button>          
+          {/* <Card.Content>{extra}</Card.Content> */}
+        </Card>
+      );
+    });
     return (
-      <Card key={recipe.id} as="a" href={link}>
-        <Image
-          src={recipe.image}
-          wrapped
-          ui={false}
-          target="_blank"
-        />
-        <Card.Content>
-          <Card.Header>{recipe.title}</Card.Header>
-          <Card.Meta>
-            <span>
-              {recipe.categories.map((category) => category.category)}
-            </span>
-          </Card.Meta>
-          <Card.Description>{recipe.description}</Card.Description>
-          <Card.Meta>
-            <span>Submitted by: {recipe.createdBy}</span>
-          </Card.Meta>
-        </Card.Content>
-        {/* <Card.Content>{extra}</Card.Content> */}
-      </Card>
+      <Container>
+        <Responsive maxWidth="780">
+          <Card.Group itemsPerRow={1}>{recipes}</Card.Group>
+        </Responsive>
+        <Responsive minWidth="781">
+          <Card.Group itemsPerRow={2}>{recipes}</Card.Group>
+        </Responsive>
+      </Container>
     );
-  });
-  return (
-    <Container>
-      <Responsive maxWidth="780">
-        <Card.Group itemsPerRow={1}>{recipes}</Card.Group>
-      </Responsive>
-      <Responsive minWidth="781">
-        <Card.Group itemsPerRow={2}>{recipes}</Card.Group>
-      </Responsive>
-    </Container>
-  );
+  }
 };
 export default ProfileRecipes;
