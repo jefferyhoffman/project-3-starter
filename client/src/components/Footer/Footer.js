0665f3184c6589dfc9513cd2208bea5e6cda07b6
Jacob LaMarre <lordaufchaos@yahoo.com>
2020-07-14 11:56:09 -0400

    facbook iframe

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -21,6 +21,7 @@ const Footer = () => {
                      <a href={link} target='_blank'></a>
                   )}
                </ShareLink>
+               <iframe src="https://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Fletsguess.herokuapp.com%2F&layout=button&size=large&width=77&height=28&appId" width="77" height="28" style={{border:"none",overflow:"hidden"}} scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
                <p><strong>Avatars created at<a href="https://www.cartoonify.de/" target="blank"> Cartoonify</a></strong></p>
                {/* <ShareLink link='https://your-site.com/some-page'>
                   {link => (
