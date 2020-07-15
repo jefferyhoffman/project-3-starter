ba490612eae0421e21a23c05ab2293ad3cc48e0c
Lindsey Lawson <lynseahoss@gmail.com>
2020-07-14 20:14:42 -0400

    added new avatar & gave footer background color for lets guess & avatars made at..

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
 
 
             <div className='column'>
-               <p><strong>Let's Guess</strong></p>
+               <p><strong className="footerBack">Let's Guess</strong></p>
                <p>&copy; Copyright 2020</p>
                
                
@@ -22,7 +22,7 @@ const Footer = () => {
                   )}
                </ShareLink>
                <iframe src="https://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Fletsguess.herokuapp.com%2F&layout=button&size=large&width=77&height=28&appId" width="77" height="28" style={{border:"none",overflow:"hidden" }}scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
-               <p><strong>Avatars created at<a href="https://www.cartoonify.de/" target="blank"> Cartoonify</a></strong></p>
+               <p ><strong className="footerBack">Avatars created at<a href="https://www.cartoonify.de/" target="blank"> Cartoonify</a></strong></p>
                {/* <ShareLink link='https://your-site.com/some-page'>
                   {link => (
                      <button className="button is-small is-info" href={link} target='_blank'><strong>FB</strong></button>
