c81bc5fc9e23875add6395175ef72d8311c4b8fd
TyshiaGray <tyshiagray@Tyshias-iMac.local>
2020-10-02 10:32:54 -0400

    intial setup

--- server/controllers/api/secretsController.js
+++ server/controllers/api/secretsController.js
@@ -4,14 +4,16 @@ const db = require('../../models')
 const { JWTVerifier } = require('../../lib/passport');
 
 secretsController.get('/', JWTVerifier, (req, res) => {
-
   db.Secrets.find({})
-    .then(results => {
-      res.json(results);
-    })
-    .catch(error => {
-      if (error) throw error
-    })
+  .then(results => {
+    res.json(results);
+  })
+  .catch(error => {
+    if (error) throw error
+  })
 })
+secretsController.post('/',((req, res)=>{
+  db.Secrets.create({}).then(resuslt=>res.sendStatus(200))
+}))
 
 module.exports = secretsController;
