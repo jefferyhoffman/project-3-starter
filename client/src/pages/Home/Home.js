3a72c6dbeee79aac8a9725c5cced9477720c1abe
gkourako <georgekourakos4@gmail.com>
2020-10-05 22:12:05 -0400

    No where near done with the Carousel

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 // import Hero from '../../components/Hero/Hero'
 import { Row, Col, Container, Image, Jumbotron } from 'react-bootstrap'
 import clapping from '../../pages/Images/clapping.jpg'
+import Carousel from '../../components/Carousel/Carousel'
 import "../Home/home.css"
 class HomePage extends Component {
 
   render() {
     return (
-      
+      <>
       <Jumbotron>
         <Image src={clapping} fluid/>
-        </Jumbotron>
-     
+      </Jumbotron>
+      <Carousel />
+      </>
     
-    );
-  }
+    
+    )
+}
 }
 export default HomePage;
