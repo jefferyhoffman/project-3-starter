be92e66006921296cf39169fac8e4b28d50c9242
Scott Buonya <scottbuonya@gmail.com>
2020-10-06 10:58:44 -0400

    pushing to my branch and pulling from master

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
+import { Jumbotron, Container } from 'react-bootstrap'
+import Jumping from '../../images/jumping.jpg'
 
-import logo from './logo.svg';
+const style = {
+  Jumbotron: {
+    backgroundSize: 'cover',
+    height: '50vH',
+    backgroundRepeat: 'no-repeat, center',
+    backgroundImage: `url(${ Jumping })`,
+    width: '100%'
+  }
+}
+
 
 class HomePage extends Component {
   render() {
     return (
-      <div className='Home'>
-        <div className='row'>
-          <div className='col'>
-            <img src={logo} className='App-logo' alt='logo' />
-            <p>
-              Edit <code>src/pages/Home.js</code> and save to reload.
-            </p>
-            <a
-              className='App-link'
-              href='https://reactjs.org'
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </div>
-        </div>
-      </div>
+      <Jumbotron Jumbotron-fluid style={style.Jumbotron}>
+  <Container>
+    <h1 style={{ color: 'white' }}>Fluid jumbotron</h1>
+    <p style={{ color: 'white' }}>
+      This is a modified jumbotron that occupies the entire horizontal space of
+      its parent.
+    </p>
+  </Container>
+</Jumbotron>
     );
   }
 }
