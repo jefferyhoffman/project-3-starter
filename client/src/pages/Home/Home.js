fc69cc59621bef9d63a24a7f5bf23e9c8945e848
gkourako <georgekourakos4@gmail.com>
2020-10-06 11:58:24 -0400

    added mdb react, finished footer, working on carousel

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 // import Hero from '../../components/Hero/Hero'
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
 // import clapping from '../../pages/Images/clapping.jpg'
 import "../Home/home.css";
 import Hero from "../../components/Hero/Hero";
@@ -11,20 +11,22 @@ import Carousel from '../../components/Carousel/Carousel'
 class HomePage extends Component {
   render() {
     return (
-      
-<>
-      <Jumbotron>
-        <Image src={clapping} fluid/>
-        </Jumbotron>
 
-      <div>
-        <Hero />
-        <hr className="solid"></hr>
-        <Card/>
-        <hr className="solid"></hr>
-        <Carousel />
-      </div>
-</>
+      <>
+        <div>
+          <Hero />
+          <hr className="solid"></hr>
+          <Card />
+          <hr className="solid"></hr>
+        </div>
+        <Container className="mike">
+        <Row>
+          <Col>
+            <Carousel />
+          </Col>
+        </Row>
+        </Container>
+      </>
 
 
     );
