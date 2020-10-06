d286a7a57344451d70b04118d851351bab282f20
Kmarzi <k.l.marziali@gmail.com>
2020-10-06 19:55:42 -0400

    added cards and images to homepage

---
+++ client/src/components/Cards/Cards.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+function Cards() {
+    return (
+        <section className="section">
+            <div className="container">
+                <div className="columns">
+                    <div className="card column">
+                        <h5 className="card-title"><strong>Did you know that...</strong></h5>
+                        <img src="" className="card-img-top" alt="" />
+                        <div className="card-body"> "Depression affects over 18 million adults in any given year."" <br />
+              "Depression is the leading cause of disability for ages 15-44"<br />
+              "Depression is the primary reason why someone dies of suicide about every 12 minutes." <br />
+              YOU ARE NOT ALONE!
+            </div>
+                    </div>
+                    <div className="card column">
+                        <h5 className="card-title"></h5>
+                        <img src="https://images.pexels.com/photos/697243/pexels-photo-697243.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" className="card-img-top" alt="" />
+                        <div className="card-body"> 
+            </div>
+                    </div>
+                    <div className="card column">
+                        <h5 className="card-title"><strong>Daily Affirmation:</strong></h5>
+                        <img src="" className="card-img-top" alt="" />
+                        <div className="card-body"> Apply API for Daily Affirmations Here
+              </div>
+                    </div>
+                </div>
+                <div className="columns">
+                    <div className="card column">
+                        <h5 className="card-title"></h5>
+                        <img src="https://images.pexels.com/photos/214574/pexels-photo-214574.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" className="card-img-top" alt="" />
+                        <div className="card-body"> 
+            </div>
+                    </div>
+                    <div className="card column">
+                        <h5 className="card-title"><strong>Resources:</strong></h5>
+                        <img src="" className="card-img-top" alt="" />
+                        <div className="card-body"> If you or someone you know is seeking help,
+                        follow the link for resources. You can also use the chat box.
+            </div>
+                    </div>
+                    <div className="card column">
+                        <h5 className="card-title"></h5>
+                        <img src="https://images.pexels.com/photos/2531353/pexels-photo-2531353.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" className="card-img-top" alt="" />
+                        <div className="card-body"> 
+              </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    )
+}
+
+export default Cards;
