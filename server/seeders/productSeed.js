1a1c6b06828eda0ea4f4fda638629cb6427d8aae
Aleck Tuluenga <aleckbryson@gmail.com>
2020-10-06 20:34:57 -0700

    final men and women shirts pushed

--- server/seeders/productSeed.js
+++ server/seeders/productSeed.js
@@ -135,6 +135,152 @@ const productSeed = [
         sold: 0,
         photo: "MetalVentTechShortSleeve2.0.jpeg"
     },
+    {
+        name: "Fast and Free Short Sleeve",
+        description: "Take it further than the finish line. Keep it going in this breathable run top made with super lightweight mesh fabric.",
+        price: 35.00,
+        category: ["Shirts"],
+        inventory: [
+            {
+                quantity: 25,
+                size: "S",
+                color: ["Graphite Grey", "Chambray", "White"],
+                gender: "Mens"
+            }
+        ],
+        sold: 0,
+        photo: "FastandFreeShortSleeve.jpeg"
+    },
+    {
+        name: "Fast and Free Short Sleeve",
+        description: "Take it further than the finish line. Keep it going in this breathable run top made with super lightweight mesh fabric.",
+        price: 35.00,
+        category: ["Shirts"],
+        inventory: [
+            {
+                quantity: 25,
+                size: "M",
+                color: ["Graphite Grey", "Chambray", "White"],
+                gender: "Mens"
+            }
+        ],
+        sold: 0,
+        photo: "FastandFreeShortSleeve.jpeg"
+    },
+    {
+        name: "Fast and Free Short Sleeve",
+        description: "Take it further than the finish line. Keep it going in this breathable run top made with super lightweight mesh fabric.",
+        price: 35.00,
+        category: ["Shirts"],
+        inventory: [
+            {
+                quantity: 25,
+                size: "L",
+                color: ["Graphite Grey", "Chambray", "White"],
+                gender: "Mens"
+            }
+        ],
+        sold: 0,
+        photo: "FastandFreeShortSleeve.jpeg"
+    },
+    {
+        name: "Engineered Warmth Long Sleeve",
+        description: "Designed to perform outdoors. This merino wool-blend long sleeve has an engineered knit that creates a 3D texture for added warmth, and four-way stretch fabric for a full range of motion.",
+        price: 79.00,
+        category: ["Shirts"],
+        inventory: [
+            {
+                quantity: 25,
+                size: "S",
+                color: ["Copper", "White", "Olive"],
+                gender: "Mens"
+            }
+        ],
+        sold: 0,
+        photo: "EngineeredWarmthLongSleeve.jpeg"
+    },
+    {
+        name: "Engineered Warmth Long Sleeve",
+        description: "Designed to perform outdoors. This merino wool-blend long sleeve has an engineered knit that creates a 3D texture for added warmth, and four-way stretch fabric for a full range of motion.",
+        price: 79.00,
+        category: ["Shirts"],
+        inventory: [
+            {
+                quantity: 25,
+                size: "M",
+                color: ["Copper", "White", "Olive"],
+                gender: "Mens"
+            }
+        ],
+        sold: 0,
+        photo: "EngineeredWarmthLongSleeve.jpeg"
+    },
+    {
+        name: "Engineered Warmth Long Sleeve",
+        description: "Designed to perform outdoors. This merino wool-blend long sleeve has an engineered knit that creates a 3D texture for added warmth, and four-way stretch fabric for a full range of motion.",
+        price: 79.00,
+        category: ["Shirts"],
+        inventory: [
+            {
+                quantity: 25,
+                size: "L",
+                color: ["Copper", "White", "Olive"],
+                gender: "Mens"
+            }
+        ],
+        sold: 0,
+        photo: "EngineeredWarmthLongSleeve.jpeg"
+    },
+    {
+        name: "Drysense Long Sleeve",
+        description: "Break a sweat. This long sleeve's abrasion-resistant mesh fabric has been developed to feel good against sweaty skin.",
+        price: 45.00,
+        category: ["Shirts"],
+        inventory: [
+            {
+                quantity: 25,
+                size: "S",
+                color: ["Gold", "Black"],
+                gender: "Mens"
+            }
+        ],
+        sold: 0,
+        photo: "DrysenseLongSleeve.jpeg"
+    },
+    {
+        name: "Drysense Long Sleeve",
+        description: "Break a sweat. This long sleeve's abrasion-resistant mesh fabric has been developed to feel good against sweaty skin.",
+        price: 45.00,
+        category: ["Shirts"],
+        inventory: [
+            {
+                quantity: 25,
+                size: "M",
+                color: ["Gold", "Black"],
+                gender: "Mens"
+            }
+        ],
+        sold: 0,
+        photo: "DrysenseLongSleeve.jpeg"
+    },
+    {
+        name: "Drysense Long Sleeve",
+        description: "Break a sweat. This long sleeve's abrasion-resistant mesh fabric has been developed to feel good against sweaty skin.",
+        price: 45.00,
+        category: ["Shirts"],
+        inventory: [
+            {
+                quantity: 25,
+                size: "L",
+                color: ["Gold", "Black"],
+                gender: "Mens"
+            }
+        ],
+        sold: 0,
+        photo: "DrysenseLongSleeve.jpeg"
+    },
+
+
 ]
 
 db.Products.deleteMany({})
