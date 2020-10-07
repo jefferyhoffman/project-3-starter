853dd37689fb284614dc64b8f93d78b1d414a4b4
Aleck Tuluenga <aleckbryson@gmail.com>
2020-10-06 18:45:19 -0700

    Mens shirts added to product seed

--- server/seeders/productSeed.js
+++ server/seeders/productSeed.js
@@ -11,18 +11,130 @@ const productSeed = [
         name: "Flow Y Longline Bra",
         description: "Feels like nothing at all. Powered by buttery-soft fabric, this long-line yoga bra has minimal seams to help keep your mind on the moment.",
         price: 19.99,
-        category: ["Bra's"],
+        category: ["Bras"],
         inventory: [
             {
                 quantity: 25,
                 size: "S",
                 color: "White",
-                gender: "Women's"
+                gender: "Womens"
             }
         ],
         sold: 0,
         photo: "lulu.jpeg"
-    }
+    },
+    {
+        name: "Flow Y Longline Bra",
+        description: "Feels like nothing at all. Powered by buttery-soft fabric, this long-line yoga bra has minimal seams to help keep your mind on the moment.",
+        price: 19.99,
+        category: ["Bras"],
+        inventory: [
+            {
+                quantity: 25,
+                size: "S",
+                color: "White",
+                gender: "Womens"
+            }
+        ],
+        sold: 0,
+        photo: "lulu.jpeg"
+    },
+    {
+        name: "License to Train Short Sleeve",
+        description: "Crank out one more set. This ant-stink, sweat-wicking short-sleeve shirt is built to keep up.",
+        price: 45.00,
+        category: ["Shirts"],
+        inventory: [
+            {
+                quantity: 25,
+                size: "S",
+                color: ["White", "Olive", "Black"],
+                gender: "Mens"
+            }
+        ],
+        sold: 0,
+        photo: "LicensetoTrainShortSleeve.jpeg"
+    },
+    {
+        name: "License to Train Short Sleeve",
+        description: "Crank out one more set. This ant-stink, sweat-wicking short-sleeve shirt is built to keep up.",
+        price: 45.00,
+        category: ["Shirts"],
+        inventory: [
+            {
+                quantity: 25,
+                size: "M",
+                color: ["White", "Olive", "Black"],
+                gender: "Mens"
+            }
+        ],
+        sold: 0,
+        photo: "LicensetoTrainShortSleeve.jpeg"
+    },
+    {
+        name: "License to Train Short Sleeve",
+        description: "Crank out one more set. This ant-stink, sweat-wicking short-sleeve shirt is built to keep up.",
+        price: 45.00,
+        category: ["Shirts"],
+        inventory: [
+            {
+                quantity: 25,
+                size: "L",
+                color: ["White", "Olive", "Black"],
+                gender: "Mens"
+            }
+        ],
+        sold: 0,
+        photo: "LicensetoTrainShortSleeve.jpeg"
+    },
+    {
+        name: "Metal Vent Tech Short Sleeve 2.0",
+        description: "Meet the ultimate training top. Seamless construction, strategic venting, and anti-stink, it was born to perform sweat after sweat after sweat.",
+        price: 45.00,
+        category: ["Shirts"],
+        inventory: [
+            {
+                quantity: 25,
+                size: "S",
+                color: ["White", "Blue", "Black"],
+                gender: "Mens"
+            }
+        ],
+        sold: 0,
+        photo: "MetalVentTechShortSleeve2.0.jpeg"
+    },
+    {
+        name: "Metal Vent Tech Short Sleeve 2.0",
+        description: "Meet the ultimate training top. Seamless construction, strategic venting, and anti-stink, it was born to perform sweat after sweat after sweat.",
+        price: 45.00,
+        category: ["Shirts"],
+        inventory: [
+            {
+                quantity: 25,
+                size: "M",
+                color: ["White", "Blue", "Black"],
+                gender: "Mens"
+            }
+        ],
+        sold: 0,
+        photo: "MetalVentTechShortSleeve2.0.jpeg"
+    },
+    {
+        name: "Metal Vent Tech Short Sleeve 2.0",
+        description: "Meet the ultimate training top. Seamless construction, strategic venting, and anti-stink, it was born to perform sweat after sweat after sweat.",
+        price: 45.00,
+        category: ["Shirts"],
+        inventory: [
+            {
+                quantity: 25,
+                size: "L",
+                color: ["White", "Blue", "Black"],
+                gender: "Mens"
+            }
+        ],
+        sold: 0,
+        photo: "MetalVentTechShortSleeve2.0.jpeg"
+    },
 ]
 
 db.Products.deleteMany({})
