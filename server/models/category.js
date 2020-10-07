e409eb81af68984d3a73631662428fc8225965d1
Matthew Courtney <matthewdc182@gmail.com>
2020-10-07 10:26:16 -0400

    updated category model and delted customer model

--- server/models/category.js
+++ server/models/category.js
@@ -1,25 +1,21 @@
 'use strict';
 
 module.exports = (sequelize, DataTypes) => {
-  const Category = sequelize.define('Category', {
-    category_id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    category_name: DataTypes.STRING,
-    category_img: DataTypes.STRING,
-    category_description: DataTypes.STRING,
-  }, {});
+	const Category = sequelize.define(
+		'Category',
+		{
+			category_name: DataTypes.STRING,
+			category_img: DataTypes.STRING,
+			category_description: DataTypes.STRING
+		},
+		{}
+	);
 
-//   Category.associate = function (models) {
-//     // associations can be defined here
-//   };
+	Category.associate = function(models) {
+		Category.belongsToMany(models.Product, {
+			through: 'Product_Categories'
+		});
+	};
 
-//   Category.prototype.comparePassword = function (challenge) {
-//     return this.password === challenge;
-//   }
-
-  return Category;
+	return Category;
 };
