e98183967f7a6ac6b1150de05bc7b893a034f9d6
ryanbrooks99 <ryanbrooks99@gmail.com>
2020-10-08 15:00:51 -0400

    updated tesimonials page

--- client/src/pages/Testimonials/Testimonials.js
+++ client/src/pages/Testimonials/Testimonials.js
@@ -3,9 +3,10 @@ import Image from 'react-bootstrap/Image'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import css from '../../pages/About/About.css'
+
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
+import CSS from '../../pages/Testimonials/Testimonials.css'
 
 
 export default Testimonials;
