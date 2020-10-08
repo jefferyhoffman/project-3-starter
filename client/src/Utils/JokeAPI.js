7e7ff10a88810404f080381b286b8964777c1ec6
Kmarzi <k.l.marziali@gmail.com>
2020-10-07 20:12:32 -0400

    added APIs for jokes and affirmations- work in progress

---
+++ client/src/Utils/JokeAPI.js
@@ -0,0 +1,9 @@
+import Axios from "axios";
+
+const BASEURL = "https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist"
+
+export default {
+    search: function(query) {
+        return Axios.get(BASEURL + query)
+    }
+}
