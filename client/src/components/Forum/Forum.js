dd9706be2fe7086abdbf877423f3ea807457a9e5
EASYMAK777 <jplocallawncare@gmail.com>
2020-10-08 18:06:51 -0400

    added console log for error posting

--- client/src/components/Forum/Forum.js
+++ client/src/components/Forum/Forum.js
@@ -5,4 +5,20 @@ class Forum extends React.Component{
     state={
 
     };
+
+    componentDidMount = () => {
+
+    };
+
+    getForumPost = () =>{
+        axios.get('/api')
+        .then((response) => {
+            const data = response.data;
+            this.setState({posts:data});
+            console.log("Data Submitted");
+        })
+        .catch(() => {
+            console.log("error posting");
+        })
+    }
 }
