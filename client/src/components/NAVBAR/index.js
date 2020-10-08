70bfdf02069147625c3867f717e3dccbe4a3b233
Chris Collins <ccollins36@icloud.com>
2020-10-08 11:15:38 -0400

    replaced class with classname

--- client/src/components/NAVBAR/index.js
+++ client/src/components/NAVBAR/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./style.css";
 function NAVBAR(){
     return(
-       <!--Navbar-->
-    <nav class="navbar navbar-light #388e3c green darken-2 text-white animated slideInLeft">
+       
+    <nav className="navbar navbar-light #388e3c green darken-2 text-white animated slideInLeft">
     {/*<!-- Navbar brand -->*/}
-    <a class="navbar-brand text-white" href="#">Farm Fresh Market</a>
+    <a className="navbar-brand text-white" href="#">Farm Fresh Market</a>
 
     {/*<!-- Collapse button -->*/}
     <button
-      class="navbar-toggler toggler-example #1b5e20 green darken-4 darken-3 text-white"
+      className="navbar-toggler toggler-example #1b5e20 green darken-4 darken-3 text-white"
       type="button"
       data-toggle="collapse"
       data-target="#navbarSupportedContent41"
@@ -17,26 +17,26 @@ function NAVBAR(){
       aria-expanded="false"
       aria-label="Toggle navigation"
     >
-      <span class="white-text"><i class="fas fa-bars fa-1x"></i></span>
+      <span className="white-text"><i class="fas fa-bars fa-1x"></i></span>
     </button>
 
     {/*<!-- Collapsible content -->*/}
-    <div class="collapse navbar-collapse" id="navbarSupportedContent41">
+    <div className="collapse navbar-collapse" id="navbarSupportedContent41">
       {/*<!-- Links -->*/}
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link text-white" href="#"
-            >Home <span class="sr-only">(current)</span></a
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
+          <a className="nav-link text-white" href="#"
+            >Home <span className="sr-only">(current)</span></a
           >
         </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="#">Map</a>
+        <li className="nav-item">
+          <a className="nav-link text-white" href="#">Map</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="#">Farmers</a>
+        <li className="nav-item">
+          <a className="nav-link text-white" href="#">Farmers</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="#">Requests</a>
+        <li className="nav-item">
+          <a className="nav-link text-white" href="#">Requests</a>
         </li>
       </ul>
       {/*<!-- Links -->*/}
