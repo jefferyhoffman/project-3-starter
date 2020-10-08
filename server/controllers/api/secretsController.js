1eac3c6d570ec2c56330442a29ecde1da4802d2b
Jared C. Gober <jaredcgober@gmail.com>
2020-10-08 11:48:33 -0400

    Updated README

--- server/controllers/api/secretsController.js
+++ server/controllers/api/secretsController.js
@@ -15,7 +15,7 @@ secretsController.get('/', JWTVerifier, (req, res) => {
 })
 
 secretsController.post('/',((req, res)=>{
-  db.Secrets.create({}).then(()=>res.sendStatus(200))
+  db.Secrets.create({}).then(()=>res.sendStatus(200)).catch(err => res.status(500).json(err))
 }))
 
 module.exports = secretsController;
