fa9c45fd8a9c4f7b891f2303dce7dcc84ab94f08
EASYMAK777 <jplocallawncare@gmail.com>
2020-10-08 19:19:33 -0400

    importing react-google-location

--- client/src/pages/Clinic/Clinic.js
+++ client/src/pages/Clinic/Clinic.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Clinic extends Component {
     render() {
       return (
-       <div></div>
+       <div><h1>These are mental health clinics near your location</h1></div>
       );
     }
   }
