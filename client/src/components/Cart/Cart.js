426cc1a4d86eeaa1de935f1345fad38dc252bc34
Adam Grossman <adgrossm@gmail.com>
2020-10-09 16:56:14 -0400

    added all fields to signup page, data is pushed to database

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { CartContext } from "../../contexts/CartContext";
 import { Tshirt } from "../Tshirt/Tshirt";
 import { TshirtList } from "../Tshirt/TshirtList";
+import API from "../../lib/API";
 
 export const Cart = () => {
 
@@ -38,6 +39,29 @@ export const Cart = () => {
         }
     }
 
+    const checkout = () => {
+        console.log("checkout")
+        console.log(checkout)
+        return (
+            <div>
+            <form>
+            <ul>
+            <input type="text" id="first-name" className="form-control validate" placeholder="First Name" />
+             {/* <li>first={user.first}</li>
+             <li> last={user.last}</li>
+             <li>address={user.address}</li>
+             <li> city={user.city}</li>
+             <li>state={user.state}</li>
+             <li>zip={user.zip}</li>
+            <li>country={user.country}</li>
+            <li>phone={user.phone}</li> */}
+             
+
+            </ul>
+            </form>
+            </div>
+        )
+    }
     return ( 
         <div>
             <br />
@@ -54,7 +78,8 @@ export const Cart = () => {
                     </div>
                 )}
             <br />
-            <span>Total price : {"$"}{cart.reduce((acc, item) => acc + item.price * item.quantity, 0).toFixed(2)} </span>     
+            <span>Total price : {"$"}{cart.reduce((acc, item) => acc + item.price * item.quantity, 0).toFixed(2)} </span>   
+            <button onClick={checkout}>Checkout</button>  
         </div>  
     )
 }
--- server/controllers/api/api-order-routes.js
+++ server/controllers/api/api-order-routes.js
@@ -1,18 +1,66 @@
 const apiOrderRoutes = require("express").Router();
 const db = require('../../models');
+const { JWTVerifier } = require('../../lib/passport');
 
-// apiOrderRoutes.post("/api/orders", async (req, res) => {
+// get all orders 
+// apiOrderRoutes.get("/", function (req, res)  {
+//  db.Order.findAll().then(function(results) {
+//     res.json(results)
+// })    
+// });
+
+// show one order for specific user
+apiOrderRoutes.get("/", JWTVerifier, function (req, res)  {
+    db.Order.findAll({
+        where: {
+            UserId: req.user.id
+        }
+    }).then(function(results) {
+       res.json(results)
+   })    
+   });
+
+//    find latest orders from userId and product
+apiOrderRoutes.get("/latest", JWTVerifier, function (req, res)  {
+    db.Order.findAll({
+        where: {
+            UserId: req.user.id
+        },
+        order: [['createdAt', 'DESC']],
+        limit: 1,
+        include: {
+            model: db.Product,
+            through: { attributes: []}
+        }
+    }).then(data => {
+        res.json(data)
+    }).catch(err => {
+        console.log(err)
+        res.json(err)
+    })
+
+   })    
+   
+
+
+// creates an order for a specific user
+// apiOrderRoutes.post("/", JWTVerifier, function (req, res) {
 //     if (
-//         !req.body.name ||
-//         !req.body.email ||
+//         !req.body.first ||
+//         !req.body.last ||
+//         !req.body.email||
 //         !req.body.address||
-//         !req.body.total ||
-//         !req.body.cartItems 
+//         !req.body.city||
+//         !req.body.state||
+//         !req.body.zip||
+//         !req.body.country ||
+//         !req.body.phone 
 //     ) {
 //         return res.send({message: "Data is required." });
+//     } else {
+//       db.Order_Products(req.body).create();
+//         res.json(order); 
 //     }
-//       const order = await db.OrderDetails(req.body).save();
-//         res.send(order); 
 // });
 
 // apiOrderRoutes.get("/api/orders", async (req, res) => {
