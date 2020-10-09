fb0b13e6b833d97818e78ba67069e009195a3c8e
EASYMAK777 <jplocallawncare@gmail.com>
2020-10-09 10:45:16 -0400

    adding google maps

--- client/src/pages/Clinic/Clinic.js
+++ client/src/pages/Clinic/Clinic.js
@@ -17,12 +17,12 @@
 //       place: null,
 //     };
 //   }
- 
+
 //   render() {
 //     return (
 //       <div >
 //          <GoogleComponent
-         
+
 //           apiKey={URL}
 //           language={'en'}
 //           country={'country:us'}
@@ -31,13 +31,13 @@
 //           locationListStyle={'custom-style-list'}
 //           onChange={(e) => { this.setState({ place: e }) }} />
 //       </div>
- 
-  
+
+
 //     )
 //   } 
 // }
- 
- 
+
+
 // export default Clinic;
 
 
@@ -47,12 +47,45 @@
 
 
 
-// class Clinic extends Component {
-//     render() {
-//       return (
-//        <div><h1>These are mental health clinics near your location</h1></div>
-//       );
-//     }
-//   }
-  
-//   export default Clinic;
+
+import {Component} from 'react';
+import React from 'react';
+import {
+    withScriptjs,
+    withGoogleMap,
+    GoogleMap,
+    Marker,
+} from "react-google-maps";
+
+
+class Clinic extends Component {
+    render() {
+
+        const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+            <GoogleMap
+                defaultZoom={8}
+                defaultCenter={{ lat: -34.397, lng: 150.644 }}
+            >
+                <Marker
+                    position={{ lat: -34.397, lng: 150.644 }}
+                />
+            </GoogleMap>
+        ));
+
+
+        return (
+
+            <MapWithAMarker
+                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `400px` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+            />
+
+
+
+        );
+    }
+}
+
+export default Clinic;
