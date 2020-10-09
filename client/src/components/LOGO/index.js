781708d2ad12d697f26fc186416e562f8c59398b
Chris Collins <ccollins36@icloud.com>
2020-10-09 14:05:49 -0400

    add logo component

--- client/src/components/LOGO/index.js
+++ client/src/components/LOGO/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./style.css";
 function LOGO(){
-   
+  <img
+  src="farmFreshMarketLogo2.png"
+  class="img-fluid"
+  alt="Farm Fresh Market Logo 2"
+  id="farmFreshMarketLogo2"
+/>
 }
 export default LOGO;
