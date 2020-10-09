3b1dbd87be1fc1204913734bb87d1e775621e433
EASYMAK777 <jplocallawncare@gmail.com>
2020-10-09 10:58:58 -0400

    got the map to show up with error

--- client/src/pages/Clinic/Clinic.js
+++ client/src/pages/Clinic/Clinic.js
@@ -76,7 +76,7 @@ class Clinic extends Component {
         return (
 
             <MapWithAMarker
-                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
+                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDkjKtkKSeldM9LJo6xy3E3L0IEYIBV96M&v=3.exp&libraries=geometry,drawing,places"
                 loadingElement={<div style={{ height: `100%` }} />}
                 containerElement={<div style={{ height: `400px` }} />}
                 mapElement={<div style={{ height: `100%` }} />}
--- client/src/pages/NotFound/NotFound.js
+++ client/src/pages/NotFound/NotFound.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
 
-export default () => (
-  <div className='NotFound'>
-    <h1>Oops!</h1>
-    <p>
-      Sorry, the page you requested cannot be found. <Link to='/'>Click
-      here</Link> to return to the home page.
-    </p>
-  </div>
-);
+// export default () => (
+//   <div className='NotFound'>
+//     <h1>Oops!</h1>
+//     <p>
+//       Sorry, the page you requested cannot be found. <Link to='/'>Click
+//       here</Link> to return to the home page.
+//     </p>
+//   </div>
+// );
+
+
+import {Component} from 'react';
+import React from 'react';
+import {
+    withScriptjs,
+    withGoogleMap,
+    GoogleMap,
+    Marker,
+} from "react-google-maps";
+
+
+class Clinic extends Component {
+    render() {
+
+        const MapWithAMarker = withScriptjs(withGoogleMap(props =>
+            <GoogleMap
+                defaultZoom={8}
+                defaultCenter={{ lat: -34.397, lng: 150.644 }}
+            >
+                <Marker
+                    position={{ lat: -34.397, lng: 150.644 }}
+                />
+            </GoogleMap>
+        ));
+
+
+        return (
+
+            <MapWithAMarker
+                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDkjKtkKSeldM9LJo6xy3E3L0IEYIBV96M&v=3.exp&libraries=geometry,drawing,places"
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `400px` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+            />
+
+
+
+        );
+    }
+}
+
+export default Clinic;
