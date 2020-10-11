50443762ecc656ed69ef2840d6145cd40290eb50
EASYMAK777 <jplocallawncare@gmail.com>
2020-10-11 13:22:18 -0400

    added search bar to clinics map

--- client/src/pages/Clinic/Clinic.js
+++ client/src/pages/Clinic/Clinic.js
@@ -1,4 +1,22 @@
-import React, { useEffect, useRef } from 'react'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import React, { useEffect, useRef, } from 'react'
+
+
+
+
 
 
 console.log(process.env)
@@ -6,27 +24,40 @@ export default function Map({ options, onMount, className }) {
     const props = { ref: useRef(), className }
     const onLoad = () => {
         const map = new window.google.maps.Map(props.ref.current, options)
+        navigator.geolocation.getCurrentPosition(function (position) {
+           
+        console.log(position)
+        })
+
         onMount && onMount(map)
     }
 
+
     useEffect(() => {
-       
+
         if (!window.google) {
             const script = document.createElement(`script`)
             script.type = `text/javascript`
-            script.src = `https://maps.google.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}`
+            script.src = `https://maps.google.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&libraries=places`
 
             const headScript = document.getElementsByTagName(`script`)[0]
             headScript.parentNode.insertBefore(script, headScript)
             script.addEventListener(`load`, onLoad)
             return () => script.removeEventListener(`load`, onLoad)
         } else onLoad()
+
+
     })
 
-    
+
+
+
+
+
 
     return (
-
+        <div>
+        
         <
             div
             {...props}
@@ -34,14 +65,17 @@ export default function Map({ options, onMount, className }) {
             ={{ height: `70vh`, margin: `1em 0`, borderRadius: `0.5em` }}
         />
 
+            <input type="text" className="input" placeholder="Search..."/>
+        </div>
     )
 }
 
+
+
 Map.defaultProps = {
     options: {
-        center: { lat: 35, lng: -80 },
-        zoom: 5,
+        center: { lat: 35.2271, lng: -80.8431 },
+        zoom: 13,
     },
 }
 
-// working on doctor locations
