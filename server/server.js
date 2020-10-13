407281a52525a7d236390025ab6fb040aa9f21ca
Tyshia Gray <tyshiagray@gmail.com>
2020-10-13 15:37:54 -0400

    added deployment for heroku

--- server/server.js
+++ server/server.js
@@ -27,9 +27,14 @@ const app = express();
 
 //-- Mongoose Setup ----------------------------------------------------------
 mongoose.connect(
-  process.env.MONGODB_URI ||
-  'mongodb://localhost/ProjectThree'
-)
+  process.env.MONGODB_URI || 'mongodb://localhost/ProjectThree',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 mongoose.connection.on('error', err => {
   console.log(`Mongoose connection err:\n${err}`)
 })
