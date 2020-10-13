05c3c89c956d249e0d362db66020945d5fe120d3
Bonus Mom <AZonaGarcia@aol.com>
2020-10-13 00:02:09 -0400

    added FavIcon to landing-page.html

--- client/src/components/MARKETCARD/index.js
+++ client/src/components/MARKETCARD/index.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "./style.css";
 function MARKETCARD(){
-    return(
     <div className="card" style="width: 18rem;">
-    <img src="marketsign.jpg" className="card-img-top" alt="...">
+    <img src="..." className="card-img-top" alt="...">
     <div className="card-body">
       <h5 className="card-title">Market</h5>
       <p className="card-text">Please type in your zipcode to find Farmer Markets near you.</p>
       <a href="#" className="btn btn-primary">Market Page</a>
     </div>
-    </img>
-  );
+  
 }
 export default MARKETCARD;
