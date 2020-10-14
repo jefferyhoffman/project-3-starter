e28c05a8be5512c3051902460909e88b77251e0e
ERStapleton <68613605+ERStapleton@users.noreply.github.com>
2020-10-13 18:09:38 -0600

    Update patrondb.js

--- patrondb.js
+++ patrondb.js
@@ -13,3 +13,22 @@ password: {
 }); 
 var Patron = mongoose.model('Patron', patronSchema);
 module.exports = Patron; 
+
+if (req.body.email &&
+    req.body.password &&
+    req.body.passwordConf) {
+
+        var PatronData = {
+            email: req.body.email,
+            password: req.body.password,
+        }
+
+        Patron.create(Data, function (err,user) { 
+            if (err) {
+                return next (err)
+            } else {
+                return res.redirect('/home');
+            }
+        });
+    }
+    
