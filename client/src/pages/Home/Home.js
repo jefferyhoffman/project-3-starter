9feabd2bef7842ae4faf276dd784eb54e8bbfec6
Bonus Mom <AZonaGarcia@aol.com>
2020-10-13 20:54:37 -0400

    fixed sticky footer covering bottom of page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -5,6 +5,7 @@ import PATRONCARD from "../../components/PATRONCARD";
 import VENDORCARD from "../../components/VENDORCARD";
 import MARKETCARD from "../../components/MARKETCARD";
 import Navigation from "../../components/NAVBAR";
+import "./style.css";
 
 class HomePage extends Component {
   render() {
